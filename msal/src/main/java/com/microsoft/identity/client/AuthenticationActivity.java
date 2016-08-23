5aeab26bcc867d48668266bfada6e532b35097b6
Wei Jia <weij@microsoft.com>
2016-08-22 23:35:13 -0700

    remove fragment and use AuthenticationActivity directly

--- msal/src/main/java/com/microsoft/identity/client/AuthenticationActivity.java
+++ msal/src/main/java/com/microsoft/identity/client/AuthenticationActivity.java
@@ -26,29 +26,32 @@ package com.microsoft.identity.client;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
 
 /**
  * Custom tab requires the device to have a browser with custom tab support, chrome with version >= 45 comes with the
  * support and is available on all devices with API version >= 16 . The sdk use chrome custom tab, and before launching
  * chrome custom tab, we need to check if chrome package is in the device, if it is, it's safe to launch the chrome
- * custom tab; Otherwise the sdk will fall back to the embedded webview(TODO: what is the fallback solution, default browser or webview).
+ * custom tab; Otherwise the sdk will launch chrome(TODO: what is the fallback solution, default browser or webview).
  * AuthenticationActivity will be responsible for checking if it's safe to launch chrome custom tab, if not, will
- * create webview and launch the embedded webview.
+ * go with chrome browser, if chrome is not installed, we throw error back.
  */
 public final class AuthenticationActivity extends Activity {
 
-    private static final String TAG = AuthenticationActivity.class.getSimpleName();
-
+    private static final String TAG = AuthenticationActivity.class.getSimpleName(); //NOPMD
     private String mRequestUrl;
-
     private int mRequestId;
+    private boolean mRestarted;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // If activity is killed by the os, savedInstance will be the saved bundle.
         if (savedInstanceState != null) {
+            mRestarted = true;
             return;
         }
 
@@ -67,18 +70,16 @@ public final class AuthenticationActivity extends Activity {
 
         // We'll use custom tab if the chrome installed on the device comes with custom tab support(on 45 and above it
         // does). If the chrome package doesn't contain the support, we'll use chrome to launch the UI.
-        if (MSALUtils.getChromePackage(this.getApplicationContext()) != null) {
-            final CustomTabFragment fragment = new CustomTabFragment();
-            fragment.setRetainInstance(true);
-
-            final FragmentManager fragmentManager = this.getFragmentManager();
-            fragmentManager.beginTransaction().add(fragment, TAG).commit();
-        } else {
+        if (MSALUtils.getChromePackage(this.getApplicationContext()) == null) {
             // TODO: log that chrome is not installed, cannot prompt the UI.
             sendError(Constants.MSALError.INVALID_REQUEST, "Chrome is not installed on the device, cannot proceed with auth");
         }
     }
 
+    /**
+     * OnNewIntent will be called before onResume.
+     * @param intent
+     */
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
@@ -89,6 +90,43 @@ public final class AuthenticationActivity extends Activity {
         returnToCaller(Constants.UIResponse.AUTH_CODE_COMPLETE,
                 resultIntent);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mRestarted) {
+            cancelRequest();
+            return;
+        }
+
+        mRestarted = true;
+
+        final String chromePackageWithCustomTabSupport = MSALUtils.getChromePackageWithCustomTabSupport(
+                this.getApplicationContext());
+        final boolean isCustomTabDisabled = this.getIntent().getBooleanExtra(InteractiveRequest.DISABLE_CHROMETAB, false);
+        mRequestUrl =  this.getIntent().getStringExtra(Constants.REQUEST_URL_KEY);
+
+        // TODO: remove the check for custom tab is disabled.
+        if (chromePackageWithCustomTabSupport != null && !isCustomTabDisabled) {
+            final CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder().build();
+            customTabsIntent.intent.setPackage(MSALUtils.getChromePackageWithCustomTabSupport(this));
+            customTabsIntent.launchUrl(this, Uri.parse(mRequestUrl));
+        } else {
+            final Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(mRequestUrl));
+            browserIntent.setPackage(MSALUtils.getChromePackage(this.getApplicationContext()));
+            browserIntent.addCategory(Intent.CATEGORY_BROWSABLE);
+            this.startActivity(browserIntent);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString(Constants.REQUEST_URL_KEY, mRequestUrl);
+    }
+
     /**
      * Return the error back to caller.
      * @param resultCode The result code to return back.
@@ -110,7 +148,8 @@ public final class AuthenticationActivity extends Activity {
 
     /**
      * Send error back to caller with the error description.
-     * @param errorDescription
+     * @param errorCode The error code to send back.
+     * @param errorDescription The error description to send back.
      */
     private void sendError(final String errorCode, final String errorDescription) {
         final Intent errorIntent = new Intent();
