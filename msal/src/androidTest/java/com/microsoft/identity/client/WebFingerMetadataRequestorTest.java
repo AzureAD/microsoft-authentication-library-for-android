ce2ee0df7ba611d45dfe242b46c2c240e65593bc
Brian Melton <brianmel@microsoft.com>
2017-01-18 11:46:30 -0500

    Adding licenses to files + package reorg for testing

--- msal/src/androidTest/java/com/microsoft/identity/client/WebFingerMetadataRequestorTest.java
+++ msal/src/androidTest/java/com/microsoft/identity/client/WebFingerMetadataRequestorTest.java
@@ -23,32 +23,12 @@
 
 package com.microsoft.identity.client;
 
-final class HttpConstants {
-    
-    /**
-     * HTTP header fields.
-     */
-    static final class HeaderField {
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.SmallTest;
 
-        /**
-         * @see <a href="https://tools.ietf.org/html/rfc1945#appendix-D.2.1">RFC-1945</a>
-         */
-        static final String ACCEPT = "Accept";
+import org.junit.runner.RunWith;
 
-        /**
-         * @see <a href="https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html">RFC-2616</a>
-         */
-        static final String CONTENT_TYPE = "Content-Type";
-    }
-
-    /**
-     * Identifiers for file formats and format contents.
-     */
-    static final class MediaType {
-
-        /**
-         * @see <a href="https://tools.ietf.org/html/rfc7159">RFC-7159</a>
-         */
-        static final String APPLICATION_JSON = "application/json";
-    }
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class WebFingerMetadataRequestorTest {
 }
--- msal/src/test/java/com/microsoft/identity/client/WebFingerMetadataRequestorTest.java
+++
@@ -1,4 +0,0 @@
-package com.microsoft.identity.client;
-
-public class WebFingerMetadataRequestorTest {
-}
