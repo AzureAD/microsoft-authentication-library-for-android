300a45b01a277a18ceed1c5d547dc4d2ff87a585
weijjia <weij@microsoft.com>
2017-04-11 18:14:27 -0700

    93: address comments

--- msal/src/androidTest/java/com/microsoft/identity/client/InteractiveRequestTest.java
+++ msal/src/androidTest/java/com/microsoft/identity/client/InteractiveRequestTest.java
@@ -59,8 +59,6 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-
-import static android.os.Build.VERSION_CODES.M;
 import static org.mockito.Mockito.mock;
 
 /**
