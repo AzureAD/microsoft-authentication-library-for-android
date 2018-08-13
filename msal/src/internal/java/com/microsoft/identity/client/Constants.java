cc8179715ad15676e17a11b014c5460e0335a82d
Brian Melton <brianmel@microsoft.com>
2018-08-13 09:54:37 -0700

    Bufixing revised constants

--- msal/src/internal/java/com/microsoft/identity/client/Constants.java
+++ msal/src/internal/java/com/microsoft/identity/client/Constants.java
@@ -23,6 +23,8 @@
 
 package com.microsoft.identity.client;
 
+import com.microsoft.identity.common.adal.internal.AuthenticationConstants;
+
 /**
  * MSAL internal class for constants.
  */
@@ -34,13 +36,13 @@ final class Constants {
 
     public static final String REQUEST_URL_KEY = "com.microsoft.identity.request.url.key";
 
-    public static final String REQUEST_ID = "com.microsoft.identity.request.id";
+    public static final String REQUEST_ID = AuthenticationConstants.Browser.REQUEST_ID;
 
     public static final String TELEMETRY_REQUEST_ID = "com.microsoft.identity.telemetry.request.id";
 
     public static final String CUSTOM_TAB_REDIRECT = "com.microsoft.identity.customtab.redirect";
 
-    public static final String AUTHORIZATION_FINAL_URL = "com.microsoft.identity.client.final.url";
+    public static final String AUTHORIZATION_FINAL_URL = AuthenticationConstants.Browser.RESPONSE_FINAL_URL;
 
     public static final String WEBVIEW_SELECTION = "com.microsoft.identity.webview.selection";
 
