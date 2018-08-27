fe1fd4fef0e40b1280c40097f220b5e808646a3f
Shane Oatman <shoatman@microsoft.com>
2018-08-27 10:16:24 -0700

    Formatting and licensing updates

--- msal/src/main/java/com/microsoft/identity/client/controllers/LocalMSALController.java
+++ msal/src/main/java/com/microsoft/identity/client/controllers/LocalMSALController.java
@@ -1,3 +1,25 @@
+//  Copyright (c) Microsoft Corporation.
+//  All rights reserved.
+//
+//  This code is licensed under the MIT License.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files(the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions :
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 package com.microsoft.identity.client.controllers;
 
 import android.content.Intent;
@@ -23,7 +45,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 
-public class LocalMSALController extends MSALController{
+public class LocalMSALController extends MSALController {
 
     private AuthorizationStrategy mAuthorizationStrategy = null;
 
@@ -37,10 +59,10 @@ public class LocalMSALController extends MSALController{
         //2) Gather authorization interactively
         AuthorizationResult result = performAuthorizationRequest(oAuth2Strategy, parameters);
 
-        if(result.getAuthorizationStatus().equals(AuthorizationStatus.SUCCESS)){
+        if (result.getAuthorizationStatus().equals(AuthorizationStatus.SUCCESS)) {
             //3) Exchange authorization code for token
             TokenResult tokenResult = performTokenRequest(oAuth2Strategy, result.getAuthorizationResponse(), parameters);
-            if(tokenResult != null && tokenResult.getSuccess()){
+            if (tokenResult != null && tokenResult.getSuccess()) {
                 //4) Save tokens in token cache
                 //saveTokens(oAuth2Strategy, getAuthorizationRequest(oAuth2Strategy, parameters), tokenResult.getTokenResponse(), parameters.getTokenCache());
             }
@@ -64,7 +86,7 @@ public class LocalMSALController extends MSALController{
 
     }
 
-    private AuthorizationRequest getAuthorizationRequest(MSALAcquireTokenOperationParameters parameters){
+    private AuthorizationRequest getAuthorizationRequest(MSALAcquireTokenOperationParameters parameters) {
         AuthorizationRequest authRequest = new MicrosoftStsAuthorizationRequest();
 
         String scopes = StringUtil.join(' ', parameters.getScopes());
