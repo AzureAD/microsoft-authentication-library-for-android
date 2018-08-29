49a437b011ce0840becd6a49a41baefc53afc0a9
Brian Melton <brianmel@microsoft.com>
2018-08-29 08:33:06 -0700

    Formatter

--- msal/src/androidTest/java/com/microsoft/identity/client/PublicClientConfigurationTest.java
+++ msal/src/androidTest/java/com/microsoft/identity/client/PublicClientConfigurationTest.java
@@ -12,11 +12,11 @@ public class PublicClientConfigurationTest {
      * Verifies that a minimum configuration containing only:
      * client_id
      * redirect_uri
-     *
+     * <p>
      * Is merged with the default configuration correctly.
      */
     @Test
-    public void testMinimumValidConfiguration(){
+    public void testMinimumValidConfiguration() {
 
     }
 
@@ -24,7 +24,7 @@ public class PublicClientConfigurationTest {
      * Verify B2C Authority set via configuration correctly
      */
     @Test
-    public void testB2CAuthorityValidConfiguration(){
+    public void testB2CAuthorityValidConfiguration() {
 
     }
 
@@ -32,7 +32,7 @@ public class PublicClientConfigurationTest {
      * Verify that unknown authority type results in exception
      */
     @Test
-    public void testUnknownAuthorityException(){
+    public void testUnknownAuthorityException() {
 
     }
 
@@ -40,7 +40,7 @@ public class PublicClientConfigurationTest {
      * Verify that unknown audience type results in exception
      */
     @Test
-    public void testUnknownAudienceException(){
+    public void testUnknownAudienceException() {
 
     }
 
@@ -48,7 +48,7 @@ public class PublicClientConfigurationTest {
      * Verify that null client id throws an exception
      */
     @Test
-    public void testNullClientIdException(){
+    public void testNullClientIdException() {
 
     }
 
@@ -56,7 +56,7 @@ public class PublicClientConfigurationTest {
      * Verify that null redirect URI throws an exception
      */
     @Test
-    public void testNullRedirectUrlException(){
+    public void testNullRedirectUrlException() {
 
     }
 
