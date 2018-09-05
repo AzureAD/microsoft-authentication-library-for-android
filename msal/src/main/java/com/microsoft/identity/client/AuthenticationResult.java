319bd3aeb81a33e24763346fa55beaf6748b2574
Brian Melton <brianmel@microsoft.com>
2018-09-05 15:46:45 -0700

    Adds accessor for Account to AuthenticationResult, updates common, updates MainActivity

--- msal/src/main/java/com/microsoft/identity/client/AuthenticationResult.java
+++ msal/src/main/java/com/microsoft/identity/client/AuthenticationResult.java
@@ -71,7 +71,7 @@ public final class AuthenticationResult {
         }
     }
 
-    public AuthenticationResult(final ICacheRecord cacheRecord){
+    public AuthenticationResult(final ICacheRecord cacheRecord) {
 
         mAccessTokenCacheItem = null;
         mUser = null;
@@ -85,13 +85,11 @@ public final class AuthenticationResult {
         mAccount = cacheRecord.getAccount();
     }
 
-
-
     /**
      * @return The access token requested.
      */
     public String getAccessToken() {
-        return mAccessTokenCacheItem.getAccessToken();
+        return null != mAccessTokenCacheItem ? mAccessTokenCacheItem.getAccessToken() : mAccessToken.getSecret();
     }
 
     /**
@@ -100,7 +98,15 @@ public final class AuthenticationResult {
      * service.
      */
     public Date getExpiresOn() {
-        return mAccessTokenCacheItem.getExpiresOn();
+        final Date expiresOn;
+
+        if (null != mAccessTokenCacheItem) {
+            expiresOn = mAccessTokenCacheItem.getExpiresOn();
+        } else {
+            expiresOn = new Date(1000L * Long.valueOf(mAccessToken.getExpiresOn()));
+        }
+
+        return expiresOn;
     }
 
     /**
@@ -123,6 +129,15 @@ public final class AuthenticationResult {
      */
     public String getIdToken() {
         return mRawIdToken;
+    }
+
+    /**
+     * Gets the Account.
+     *
+     * @return The Account to get.
+     */
+    public Account getAccount() {
+        return mAccount;
     }
 
     /**
