836b0ce9757569dd9c0ce926601de02cd5d234ca
Shane Oatman <shoatman@microsoft.com>
2018-11-12 13:37:59 -0800

    Licenses and javadoc

--- msal/src/main/java/com/microsoft/identity/client/AcquireTokenParameters.java
+++ msal/src/main/java/com/microsoft/identity/client/AcquireTokenParameters.java
@@ -1,3 +1,26 @@
+//  Copyright (c) Microsoft Corporation.
+//  All rights reserved.
+//
+//  This code is licensed under the MIT License.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files(the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions :
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
 package com.microsoft.identity.client;
 
 import android.app.Activity;
@@ -5,6 +28,9 @@ import android.util.Pair;
 
 import java.util.List;
 
+/**
+ * Encapsulates the parameters passed to the acquireToken methods of PublicClientApplication
+ */
 public class AcquireTokenParameters extends TokenParameters {
 
     private Activity mActivity;
@@ -14,42 +40,86 @@ public class AcquireTokenParameters extends TokenParameters {
     private List<Pair<String, String>> mExtraQueryStringParameters;
 
 
+    /**
+     * Non-null {@link Activity} that will be used as the parent activity for launching the {@link AuthenticationActivity}
+     * @return
+     */
     public Activity getActivity() {
         return mActivity;
     }
 
+    /**
+     * Non-null {@link Activity} that will be used as the parent activity for launching the {@link AuthenticationActivity}
+     * @param activity
+     */
     public void setActivity(Activity activity) {
         this.mActivity = activity;
     }
 
+    /**
+     * Optional. Gets the login hint sent along with the authorization request.
+     * @return
+     */
     public String getLoginHint() {
         return mLoginHint;
     }
 
+    /**
+     * Sets the login hint sent along with the authorization request.
+     * @param loginHint
+     */
     public void setLoginHint(String loginHint) {
         this.mLoginHint = loginHint;
     }
 
+    /**
+     * Controls the value of the prompt parameter sent along with the authorization request.
+     * @return
+     */
     public UiBehavior getUIBehavior() {
         return mUIBehavior;
     }
 
+    /**
+     * Controls the value of the prompt parameter sent along with the authorization request.
+     * @param uiBehavior
+     */
     public void setUIBehavior(UiBehavior uiBehavior) {
         this.mUIBehavior = uiBehavior;
     }
 
+    /**
+     * These are additional scopes that you would like the user to authorize the use of, while getting consent
+     * for the first set of scopes
+     * @return
+     */
     public List<String> getExtraScopesToConsent() {
         return mExtraScopesToConsent;
     }
 
+    /**
+     * These are additional scopes that you would like the user to authorize the use of, while getting consent
+     * for the first set of scopes
+     * @param extraScopesToConsent
+     */
     public void setExtraScopesToConsent(List<String> extraScopesToConsent) {
         this.mExtraScopesToConsent = extraScopesToConsent;
     }
 
+    /**
+     * If you've been instructed to pass additional query string parameters to the authorization endpoint.  You can get these here.
+     * Otherwise... would recommend not touching.
+     * @return
+     */
     public List<Pair<String, String>> getExtraQueryStringParameters() {
         return mExtraQueryStringParameters;
     }
 
+    /**
+     * If you've been instructed to pass additional query string parameters to the authorization endpoint.  You can set these here.
+     * Otherwise... would recommend not touching.
+     * @param extraQueryStringParameters
+     */
     public void setExtraQueryStringParameters(List<Pair<String, String>> extraQueryStringParameters) {
         this.mExtraQueryStringParameters = extraQueryStringParameters;
     }
--- msal/src/main/java/com/microsoft/identity/client/TokenParameters.java
+++ msal/src/main/java/com/microsoft/identity/client/TokenParameters.java
@@ -1,3 +1,26 @@
+//  Copyright (c) Microsoft Corporation.
+//  All rights reserved.
+//
+//  This code is licensed under the MIT License.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files(the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions :
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
 package com.microsoft.identity.client;
 
 import com.microsoft.identity.client.claims.ClaimsRequest;
@@ -5,6 +28,9 @@ import com.microsoft.identity.common.internal.dto.AccountRecord;
 
 import java.util.List;
 
+/**
+ * Base class for AcquireTokenParameters and AcquireTokenSilentParameters
+ */
 abstract class TokenParameters {
     
     private List<String> mScopes;
@@ -15,42 +41,102 @@ abstract class TokenParameters {
     private AccountRecord mAccountRecord;
 
 
+    /**
+     * The non-null array of scopes to be requested for the access token.
+     * MSAL always sends the scopes 'openid profile offline_access'.  Do not include any of these scopes in the scope parameter.
+     *
+     * @return
+     */
     public List<String> getScopes() {
         return mScopes;
     }
 
+    /**
+     * The non-null array of scopes to be requested for the access token.
+     * MSAL always sends the scopes 'openid profile offline_access'.  Do not include any of these scopes in the scope parameter.
+     *
+     * @param scopes
+     */
     public void setScopes(List<String> scopes) {
         this.mScopes = scopes;
     }
 
+    /**
+     * Optional. If provided, will be used to force the session continuation.  If user tries to sign in with a different account, error
+     * will be returned.
+     *
+     * @return
+     */
     public IAccount getAccount() {
         return mAccount;
     }
 
+    /**
+     * Optional. If provided, will be used to force the session continuation.  If user tries to sign in with a different account, error
+     * will be returned.
+     *
+     * @param account
+     */
     public void setAccount(IAccount account) {
         this.mAccount = account;
     }
 
+    /**
+     * Optional. Can be passed to override the default authority.
+     * @return
+     */
     public String getAuthority() {
         return mAuthority;
     }
 
+    /**
+     * Optional. Can be passed to override the default authority.
+     * @param authority
+     */
     public void setAuthority(String authority) {
         this.mAuthority = authority;
     }
 
+    /**
+     * Optional. Can be passed into request specific claims in the id_token and access_token
+     * @return
+     */
     public ClaimsRequest getClaimsRequest() {
         return mClaimsRequest;
     }
 
+    /**
+     * Optional. Can be passed into request specific claims in the id_token and access_token
+     * @param claimsRequest
+     */
     public void setClaimsRequest(ClaimsRequest claimsRequest) {
         this.mClaimsRequest = claimsRequest;
     }
 
+    /**
+     * The Non-null {@link AuthenticationCallback} to receive the result back.
+     * 1) If user cancels the flow by pressing the device back button, the result will be sent
+     * back via {@link AuthenticationCallback#onCancel()}.
+     * 2) If the sdk successfully receives the token back, result will be sent back via
+     * {@link AuthenticationCallback#onSuccess(AuthenticationResult)}
+     * 3) All the other errors will be sent back via
+     * {@link AuthenticationCallback#onError(com.microsoft.identity.client.exception.MsalException)}.
+     * @return
+     */
     public AuthenticationCallback getCallback() {
         return mCallback;
     }
 
+    /**
+     * The Non-null {@link AuthenticationCallback} to receive the result back.
+     * 1) If user cancels the flow by pressing the device back button, the result will be sent
+     * back via {@link AuthenticationCallback#onCancel()}.
+     * 2) If the sdk successfully receives the token back, result will be sent back via
+     * {@link AuthenticationCallback#onSuccess(AuthenticationResult)}
+     * 3) All the other errors will be sent back via
+     * {@link AuthenticationCallback#onError(com.microsoft.identity.client.exception.MsalException)}.
+     * @param callback
+     */
     public void setCallback(AuthenticationCallback callback) {
         this.mCallback = callback;
     }
