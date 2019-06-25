a2626ac6323fc358f1b75c6ff8698012e92b1243
Shane Oatman <shoatman@microsoft.com>
2019-06-25 11:00:15 -0700

    Updates

--- msal/src/main/java/com/microsoft/identity/client/internal/CreateApplicationResult.java
+++ msal/src/main/java/com/microsoft/identity/client/internal/CreateApplicationResult.java
@@ -1,4 +1,28 @@
+//  Copyright (c) Microsoft Corporation.
+//  All rights reserved.
+//
+//  This code is licensed under the MIT License.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files(the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions :
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 package com.microsoft.identity.client.internal;
+
+import android.support.annotation.Nullable;
 
 import com.microsoft.identity.client.IPublicClientApplication;
 import com.microsoft.identity.client.exception.MsalException;
@@ -9,7 +33,7 @@ public class CreateApplicationResult  {
     private IPublicClientApplication mIPublicClientApplication;
     private Boolean mSuccess = false;
 
-    public CreateApplicationResult(IPublicClientApplication application, MsalException exception){
+    public CreateApplicationResult(@Nullable IPublicClientApplication application, @Nullable MsalException exception){
         mMsalException = exception;
         mIPublicClientApplication = application;
         if(application != null){
