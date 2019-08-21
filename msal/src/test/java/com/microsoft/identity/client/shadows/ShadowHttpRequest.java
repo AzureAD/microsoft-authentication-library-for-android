470a1aefc5529be07599278588dd2ed4a2e59796
Shahzaib <shahzaib.jameel@microsoft.com>
2019-08-21 10:56:48 -0700

    Implement shadow for http request

---
+++ msal/src/test/java/com/microsoft/identity/client/shadows/ShadowHttpRequest.java
@@ -0,0 +1,22 @@
+package com.microsoft.identity.client.shadows;
+
+import com.microsoft.identity.common.internal.net.HttpRequest;
+import com.microsoft.identity.common.internal.net.HttpResponse;
+
+import org.robolectric.annotation.Implements;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+
+@Implements(HttpRequest.class)
+public class ShadowHttpRequest {
+
+    // mocking this to avoid accidentally sending malformed requests to the server
+    public static HttpResponse sendPost(final URL requestUrl, final Map<String, String> requestHeaders,
+                                        final byte[] requestContent, final String requestContentType)
+            throws IOException {
+
+        throw new IOException("Sending requests to server has been disabled for mocked unit tests");
+    }
+}
