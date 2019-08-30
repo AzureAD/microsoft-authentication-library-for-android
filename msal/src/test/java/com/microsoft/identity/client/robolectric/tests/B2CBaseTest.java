fd4c89d01af9e5f75e9c3c72946512915533ad75
Shahzaib <shahzaib.jameel@microsoft.com>
2019-08-30 16:29:26 -0700

    Add acquire token tests for b2c

---
+++ msal/src/test/java/com/microsoft/identity/client/robolectric/tests/B2CBaseTest.java
@@ -0,0 +1,55 @@
+package com.microsoft.identity.client.robolectric.tests;
+
+import android.app.Activity;
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import com.microsoft.identity.client.IPublicClientApplication;
+import com.microsoft.identity.client.PublicClientApplication;
+import com.microsoft.identity.client.exception.MsalException;
+import com.microsoft.identity.msal.R;
+
+import org.mockito.Mockito;
+
+import java.io.File;
+
+public abstract class B2CBaseTest {
+
+    private static final String CONFIG_FILE_PATH = "src/test/res/raw/b2c_test_config.json";
+
+    abstract void makeAcquireTokenCall(final IPublicClientApplication publicClientApplication,
+                                       final Activity activity) throws InterruptedException;
+
+
+    private Activity getActivity(final Context context) {
+        final Activity mockedActivity = Mockito.mock(Activity.class);
+        Mockito.when(mockedActivity.getApplicationContext()).thenReturn(context);
+
+        return mockedActivity;
+    }
+
+
+    public void performTest() {
+        final Context context = ApplicationProvider.getApplicationContext();
+        final Activity testActivity = getActivity(context);
+
+        File configFile = new File(CONFIG_FILE_PATH);
+
+        PublicClientApplication.create(context, configFile, new PublicClientApplication.ApplicationCreatedListener() {
+            @Override
+            public void onCreated(IPublicClientApplication application) {
+                try {
+                    makeAcquireTokenCall(application, testActivity);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onError(MsalException exception) {
+                exception.printStackTrace();
+            }
+        });
+    }
+}
---
+++ msal/src/test/java/com/microsoft/identity/client/robolectric/tests/B2CTest.java
@@ -0,0 +1,348 @@
+package com.microsoft.identity.client.robolectric.tests;
+
+import android.app.Activity;
+
+import com.microsoft.identity.client.AcquireTokenParameters;
+import com.microsoft.identity.client.AcquireTokenSilentParameters;
+import com.microsoft.identity.client.AuthenticationCallback;
+import com.microsoft.identity.client.IAccount;
+import com.microsoft.identity.client.IAuthenticationResult;
+import com.microsoft.identity.client.IPublicClientApplication;
+import com.microsoft.identity.client.exception.MsalClientException;
+import com.microsoft.identity.client.exception.MsalException;
+import com.microsoft.identity.client.robolectric.shadows.ShadowAuthority;
+import com.microsoft.identity.client.robolectric.shadows.ShadowMsalUtils;
+import com.microsoft.identity.client.robolectric.shadows.ShadowStorageHelper;
+import com.microsoft.identity.common.internal.util.StringUtil;
+import com.microsoft.identity.common.utilities.TestConfigurationHelper;
+import com.microsoft.identity.common.utilities.TestConfigurationQuery;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.Arrays;
+
+import static junit.framework.Assert.fail;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(shadows = {ShadowStorageHelper.class, ShadowMsalUtils.class, ShadowAuthority.class})
+public final class B2CTest {
+
+    private static final String[] SCOPES = {"https://msidlabb2c.onmicrosoft.com/msidlabb2capi/read", "openid", "offline_access", "profile"};
+
+    @Test
+    public void canPerformROPC() {
+        new B2CBaseTest() {
+
+            @Override
+            void makeAcquireTokenCall(final IPublicClientApplication publicClientApplication,
+                                      final Activity activity) {
+
+                TestConfigurationQuery query = new TestConfigurationQuery();
+                query.b2cProvider = "Local";
+                String username = TestConfigurationHelper.getUpnForTest(query);
+
+                AcquireTokenParameters parameters = new AcquireTokenParameters.Builder()
+                        .startAuthorizationFromActivity(activity)
+                        .withLoginHint(username)
+                        .withScopes(Arrays.asList(SCOPES))
+                        .callback(new AuthenticationCallback() {
+                            @Override
+                            public void onSuccess(IAuthenticationResult authenticationResult) {
+                                Assert.assertTrue(!StringUtil.isEmpty(authenticationResult.getAccessToken()));
+                            }
+
+                            @Override
+                            public void onError(MsalException exception) {
+                                fail(exception.getMessage());
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                fail("User cancelled flow");
+                            }
+                        })
+                        .build();
+
+                publicClientApplication.acquireToken(parameters);
+                RoboTestUtils.flushScheduler();
+            }
+
+        }.performTest();
+    }
+
+    @Test
+    public void canAcquireSilentAfterGettingToken() {
+        new B2CBaseTest() {
+
+            @Override
+            void makeAcquireTokenCall(final IPublicClientApplication publicClientApplication,
+                                      final Activity activity) {
+
+                TestConfigurationQuery query = new TestConfigurationQuery();
+                query.b2cProvider = "Local";
+
+                final String username = TestConfigurationHelper.getUpnForTest(query);
+
+                final AcquireTokenSilentParameters silentParameters = new AcquireTokenSilentParameters.Builder()
+                        .withScopes(Arrays.asList(SCOPES))
+                        .forceRefresh(false)
+                        .callback(new AuthenticationCallback() {
+                            @Override
+                            public void onSuccess(IAuthenticationResult authenticationResult) {
+                                Assert.assertTrue(!StringUtil.isEmpty(authenticationResult.getAccessToken()));
+                            }
+
+                            @Override
+                            public void onError(MsalException exception) {
+                                fail(exception.getMessage());
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                fail("User cancelled flow");
+                            }
+                        })
+                        .build();
+
+                AcquireTokenParameters parameters = new AcquireTokenParameters.Builder()
+                        .startAuthorizationFromActivity(activity)
+                        .withLoginHint(username)
+                        .withScopes(Arrays.asList(SCOPES))
+                        .callback(new AuthenticationCallback() {
+                            @Override
+                            public void onSuccess(IAuthenticationResult authenticationResult) {
+                                Assert.assertTrue(!StringUtil.isEmpty(authenticationResult.getAccessToken()));
+                                IAccount account = authenticationResult.getAccount();
+                                silentParameters.setAccount(account);
+                            }
+
+                            @Override
+                            public void onError(MsalException exception) {
+                                fail(exception.getMessage());
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                fail("User cancelled flow");
+                            }
+                        })
+                        .build();
+
+                publicClientApplication.acquireToken(parameters);
+                RoboTestUtils.flushScheduler();
+                publicClientApplication.acquireTokenSilentAsync(silentParameters);
+                RoboTestUtils.flushScheduler();
+            }
+
+        }.performTest();
+    }
+
+    @Test
+    public void forceRefreshWorks() {
+        new B2CBaseTest() {
+
+            @Override
+            void makeAcquireTokenCall(final IPublicClientApplication publicClientApplication,
+                                      final Activity activity) {
+
+                TestConfigurationQuery query = new TestConfigurationQuery();
+                query.b2cProvider = "Local";
+
+                final String username = TestConfigurationHelper.getUpnForTest(query);
+
+                final AcquireTokenSilentParameters silentParameters = new AcquireTokenSilentParameters.Builder()
+                        .withScopes(Arrays.asList(SCOPES))
+                        .forceRefresh(true)
+                        .callback(new AuthenticationCallback() {
+                            @Override
+                            public void onSuccess(IAuthenticationResult authenticationResult) {
+                                Assert.assertTrue(!StringUtil.isEmpty(authenticationResult.getAccessToken()));
+                            }
+
+                            @Override
+                            public void onError(MsalException exception) {
+                                fail(exception.getMessage());
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                fail("User cancelled flow");
+                            }
+                        })
+                        .build();
+
+                AcquireTokenParameters parameters = new AcquireTokenParameters.Builder()
+                        .startAuthorizationFromActivity(activity)
+                        .withLoginHint(username)
+                        .withScopes(Arrays.asList(SCOPES))
+                        .callback(new AuthenticationCallback() {
+                            @Override
+                            public void onSuccess(IAuthenticationResult authenticationResult) {
+                                Assert.assertTrue(!StringUtil.isEmpty(authenticationResult.getAccessToken()));
+                                IAccount account = authenticationResult.getAccount();
+                                silentParameters.setAccount(account);
+                            }
+
+                            @Override
+                            public void onError(MsalException exception) {
+                                fail(exception.getMessage());
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                fail("User cancelled flow");
+                            }
+                        })
+                        .build();
+
+                publicClientApplication.acquireToken(parameters);
+                RoboTestUtils.flushScheduler();
+                publicClientApplication.acquireTokenSilentAsync(silentParameters);
+                RoboTestUtils.flushScheduler();
+            }
+
+        }.performTest();
+    }
+
+    @Test
+    public void silentCallFailsIfCacheIsEmpty() {
+        new B2CBaseTest() {
+
+            @Override
+            void makeAcquireTokenCall(final IPublicClientApplication publicClientApplication,
+                                      final Activity activity) {
+
+                TestConfigurationQuery query = new TestConfigurationQuery();
+                query.b2cProvider = "Local";
+
+                final String username = TestConfigurationHelper.getUpnForTest(query);
+
+                final AcquireTokenSilentParameters silentParameters = new AcquireTokenSilentParameters.Builder()
+                        .withScopes(Arrays.asList(SCOPES))
+                        .forceRefresh(false)
+                        .callback(new AuthenticationCallback() {
+                            @Override
+                            public void onSuccess(IAuthenticationResult authenticationResult) {
+                                fail("Unexpected success");
+                            }
+
+                            @Override
+                            public void onError(MsalException exception) {
+                                Assert.assertTrue(exception instanceof MsalClientException);
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                fail("User cancelled flow");
+                            }
+                        })
+                        .build();
+
+                AcquireTokenParameters parameters = new AcquireTokenParameters.Builder()
+                        .startAuthorizationFromActivity(activity)
+                        .withLoginHint(username)
+                        .withScopes(Arrays.asList(SCOPES))
+                        .callback(new AuthenticationCallback() {
+                            @Override
+                            public void onSuccess(IAuthenticationResult authenticationResult) {
+                                Assert.assertTrue(!StringUtil.isEmpty(authenticationResult.getAccessToken()));
+                                IAccount account = authenticationResult.getAccount();
+                                silentParameters.setAccount(account);
+                            }
+
+                            @Override
+                            public void onError(MsalException exception) {
+                                fail(exception.getMessage());
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                fail("User cancelled flow");
+                            }
+                        })
+                        .build();
+
+                publicClientApplication.acquireToken(parameters);
+                RoboTestUtils.flushScheduler();
+                RoboTestUtils.clearCache();
+                publicClientApplication.acquireTokenSilentAsync(silentParameters);
+                RoboTestUtils.flushScheduler();
+            }
+
+        }.performTest();
+    }
+
+    @Test
+    public void silentWorksWhenCacheHasNoAccessToken() {
+        new B2CBaseTest() {
+
+            @Override
+            void makeAcquireTokenCall(final IPublicClientApplication publicClientApplication,
+                                      final Activity activity) {
+
+                TestConfigurationQuery query = new TestConfigurationQuery();
+                query.b2cProvider = "Local";
+
+                final String username = TestConfigurationHelper.getUpnForTest(query);
+
+                final AcquireTokenSilentParameters silentParameters = new AcquireTokenSilentParameters.Builder()
+                        .withScopes(Arrays.asList(SCOPES))
+                        .forceRefresh(false)
+                        .callback(new AuthenticationCallback() {
+                            @Override
+                            public void onSuccess(IAuthenticationResult authenticationResult) {
+                                Assert.assertTrue(!StringUtil.isEmpty(authenticationResult.getAccessToken()));
+                            }
+
+                            @Override
+                            public void onError(MsalException exception) {
+                                fail(exception.getMessage());
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                fail("User cancelled flow");
+                            }
+                        })
+                        .build();
+
+                AcquireTokenParameters parameters = new AcquireTokenParameters.Builder()
+                        .startAuthorizationFromActivity(activity)
+                        .withLoginHint(username)
+                        .withScopes(Arrays.asList(SCOPES))
+                        .callback(new AuthenticationCallback() {
+                            @Override
+                            public void onSuccess(IAuthenticationResult authenticationResult) {
+                                Assert.assertTrue(!StringUtil.isEmpty(authenticationResult.getAccessToken()));
+                                IAccount account = authenticationResult.getAccount();
+                                silentParameters.setAccount(account);
+                            }
+
+                            @Override
+                            public void onError(MsalException exception) {
+                                fail(exception.getMessage());
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                fail("User cancelled flow");
+                            }
+                        })
+                        .build();
+
+                publicClientApplication.acquireToken(parameters);
+                RoboTestUtils.flushScheduler();
+                RoboTestUtils.removeAccessTokenFromCache();
+                publicClientApplication.acquireTokenSilentAsync(silentParameters);
+                RoboTestUtils.flushScheduler();
+            }
+
+        }.performTest();
+    }
+
+
+}
