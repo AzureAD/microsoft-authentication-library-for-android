ef8aa28a0a3d05315485474608e6f2e7df9d5510
Shahzaib <shahzaib.jameel@microsoft.com>
2019-09-09 15:18:11 -0700

    Address comments

--- msal/src/test/java/com/microsoft/identity/client/robolectric/shadows/ShadowAuthority.java
+++ msal/src/test/java/com/microsoft/identity/client/robolectric/shadows/ShadowAuthority.java
@@ -1,3 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 package com.microsoft.identity.client.robolectric.shadows;
 
 import android.net.Uri;
@@ -15,6 +37,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
+// A Shadow is Robolectric's way of mocking code
+// A shadow works in a similar way to method overriding
+// Implementing a shadow for a class does not mean that we are shadowing the entire class,
+// instead we are only shadowing the particular method that is implemented in the shadow
+// so in this case, the only thing that we are shadowing is the getAuthorityFromAuthorityUrl method in the Authority class
 @Implements(Authority.class)
 public class ShadowAuthority {
 
@@ -24,8 +51,8 @@ public class ShadowAuthority {
     private static final String B2C_TEST_PATH_SEGMENT = "tfp";
 
     /**
-     * Returns an Authority based on an authority url.  This method attempts to parse the URL and based on the contents of it
-     * determine the authority type and tenantid associated with it.
+     * Returns an Authority based on an authority url.  This method works in similar way to the actual
+     * method in the Authority class, except that over here we create and return test versions of the Authorities
      *
      * @param authorityUrl
      * @return
@@ -58,26 +85,14 @@ public class ShadowAuthority {
             // more cases can be added here in the future
             case AAD_MOCK_PATH_SEGMENT:
                 //Return new AAD MOCK Authority
-                Logger.verbose(
-                        TAG + methodName,
-                        "Authority type is Mock"
-                );
                 authority = new MockAuthority();
                 break;
             case B2C_TEST_PATH_SEGMENT:
                 //Return new B2C TEST Authority
-                Logger.verbose(
-                        TAG + methodName,
-                        "Authority type is B2C Test"
-                );
                 authority = new B2CTestAuthority(authorityUrl);
                 break;
             default:
                 // return new AAD Test Authority
-                Logger.verbose(
-                        TAG + methodName,
-                        "Authority type default: AAD Test"
-                );
                 authority = new AADTestAuthority();
                 break;
         }
--- msal/src/test/java/com/microsoft/identity/client/robolectric/shadows/ShadowStrategyResultServerError.java
+++ msal/src/test/java/com/microsoft/identity/client/robolectric/shadows/ShadowStrategyResultServerError.java
@@ -1,3 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 package com.microsoft.identity.client.robolectric.shadows;
 
 import com.microsoft.identity.common.internal.net.HttpResponse;
@@ -10,13 +32,17 @@ import org.robolectric.annotation.Implements;
 @Implements(MockTestStrategy.class)
 public class ShadowStrategyResultServerError {
 
+    // overriding this method from MockTestStrategy class to return a error response in this case
     public HttpResponse makeHttpResponseFromResponseObject(final Object obj) {
+        // create a tokenErrorResponse object
         final TokenErrorResponse errorResponse = createFakeTokenErrorResponse();
         final String httpResponseBody = ObjectMapper.serializeObjectToJsonString(errorResponse);
+        // create http response from error response, use 500 http code
         HttpResponse httpResponse = new HttpResponse(500, httpResponseBody, null);
         return httpResponse;
     }
 
+    // create a fake token error response
     private TokenErrorResponse createFakeTokenErrorResponse() {
         TokenErrorResponse tokenErrorResponse = new TokenErrorResponse();
         tokenErrorResponse.setError("Internal Server Error");
