4b6077bb5a257517bf16881571ca2439ab0d24c9
kreedula <kreedula@microsoft.com>
2019-11-21 15:10:02 -0800

    More changes

--- msal/src/test/java/com/microsoft/identity/client/e2e/utils/TestConstants.java
+++ msal/src/test/java/com/microsoft/identity/client/e2e/utils/TestConstants.java
@@ -40,7 +40,7 @@ public class TestConstants {
 
     public static class Authorities {
         public static final String AAD_MOCK_AUTHORITY_TENANT = "61137f02-8854-4e46-8813-664098dc9f91";
-        public static final String AAD_MOCK_AUTHORITY = "https://test.authority/" + AAD_MOCK_AUTHORITY_TENANT ;
+        public static final String AAD_MOCK_AUTHORITY = "https://login.microsoftonline.com/" + AAD_MOCK_AUTHORITY_TENANT ;
         public static final String AAD_MOCK_DELAYED_RESPONSE_AUTHORITY = "https://test.authority/mock_with_delays";
     }
 
