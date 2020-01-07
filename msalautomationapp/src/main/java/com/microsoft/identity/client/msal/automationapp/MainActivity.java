136d829ee3791495d5244aad4707a7dbd414a46f
Shahzaib <37125644+shahzaibj@users.noreply.github.com>
2020-01-06 20:27:40 -0800

    Initial commit for msal automation app

---
+++ msalautomationapp/src/main/java/com/microsoft/identity/client/msal/automationapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.microsoft.identity.client.msal.automationapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
