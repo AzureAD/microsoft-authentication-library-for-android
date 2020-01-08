0a51c33478fb7a2f056e59316f1c2c19de4a1130
Shahzaib <shahzaib.jameel@microsoft.com>
2020-01-08 15:50:15 -0800

    Updates to msal automation app

--- msalautomationapp/src/main/java/com/microsoft/identity/client/msal/automationapp/MainActivity.java
+++ msalautomationapp/src/main/java/com/microsoft/identity/client/msal/automationapp/MainActivity.java
@@ -1,7 +1,8 @@
 package com.microsoft.identity.client.msal.automationapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
