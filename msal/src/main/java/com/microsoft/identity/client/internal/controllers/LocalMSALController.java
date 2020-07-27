97fd721583643faa64a9da3292a7af67dbb27eec
Fadi Durah <t-fadura@microsoft.com>
2020-07-27 12:37:25 -0700

    Added acquireDeviceCodeFlowToken

--- msal/src/main/java/com/microsoft/identity/client/internal/controllers/LocalMSALController.java
+++ msal/src/main/java/com/microsoft/identity/client/internal/controllers/LocalMSALController.java
@@ -45,6 +45,9 @@ import com.microsoft.identity.common.internal.commands.parameters.SilentTokenCom
 import com.microsoft.identity.common.internal.controllers.BaseController;
 import com.microsoft.identity.common.internal.dto.AccountRecord;
 import com.microsoft.identity.common.internal.logging.Logger;
+import com.microsoft.identity.common.internal.providers.microsoft.microsoftsts.MicrosoftSts;
+import com.microsoft.identity.common.internal.providers.microsoft.microsoftsts.MicrosoftStsAuthorizationResponse;
+import com.microsoft.identity.common.internal.providers.microsoft.microsoftsts.MicrosoftStsTokenRequest;
 import com.microsoft.identity.common.internal.providers.oauth2.AuthorizationRequest;
 import com.microsoft.identity.common.internal.providers.oauth2.AuthorizationResult;
 import com.microsoft.identity.common.internal.providers.oauth2.AuthorizationStatus;
@@ -464,14 +467,142 @@ public class LocalMSALController extends BaseController {
 
     @Override
     public AuthorizationResult deviceCodeFlowAuthRequest(final DeviceCodeFlowCommandParameters parameters) throws Exception {
+        // Logging start of method
+        final String methodName = ":deviceCodeFlowAuthRequest";
+        Logger.verbose(
+                TAG + methodName,
+                "Device Code Flow: Authorizing user code..."
+        );
+
         // TODO: Placeholder to avoid inheritance error. Will be implemented after Command/Controller level PR in Common
+
+        // Default scopes here
+
+        // Start telemetry with LOCAL_DEVICE_CODE_FLOW_ACQUIRE_URL_AND_CODE
+        Telemetry.emit(
+                new ApiStartEvent()
+                        .putProperties(parameters)
+                        .putApiId(TelemetryEventStrings.Api.LOCAL_DEVICE_CODE_FLOW_ACQUIRE_URL_AND_CODE)
+        );
+
+        // Create OAuth2Strategy using commandParameters and strategyParameters
+        final OAuth2StrategyParameters strategyParameters = new OAuth2StrategyParameters();
+        strategyParameters.setContext(parameters.getAndroidApplicationContext());
+
+        logParameters(TAG, parameters);
+
+
+        //logResult(TAG, result);
+
+        // End telemetry with LOCAL_DEVICE_CODE_FLOW_ACQUIRE_URL_AND_CODE
+        Telemetry.emit(
+                new ApiEndEvent()
+                        .putApiId(TelemetryEventStrings.Api.LOCAL_DEVICE_CODE_FLOW_ACQUIRE_URL_AND_CODE)
+        );
+
         return null;
     }
 
     @Override
-    public AcquireTokenResult acquireDeviceCodeFlowToken(final AuthorizationResult authorizationResult, DeviceCodeFlowCommandParameters commandParameters) throws Exception {
-        // TODO: Placeholder to avoid inheritance error. Will be implemented after Command/Controller level PR in Common
-        return null;
+    public AcquireTokenResult acquireDeviceCodeFlowToken(final AuthorizationResult authorizationResult, final DeviceCodeFlowCommandParameters parameters) throws Exception {
+        // Logging start of method
+        final String methodName = ":acquireDeviceCodeFlowToken";
+        Logger.verbose(
+                TAG + methodName,
+                "Device Code Flow: Polling for token..."
+        );
+
+        // Start telemetry with LOCAL_DEVICE_CODE_FLOW_POLLING
+        Telemetry.emit(
+                new ApiStartEvent()
+                        .putApiId(TelemetryEventStrings.Api.LOCAL_DEVICE_CODE_FLOW_POLLING)
+        );
+
+        // Create empty AcquireTokenResult object
+        final AcquireTokenResult acquireTokenResult = new AcquireTokenResult();
+
+        // Assign authorization result
+        acquireTokenResult.setAuthorizationResult(authorizationResult);
+
+        // Fetch the Authorization Response
+        final MicrosoftStsAuthorizationResponse authorizationResponse = (MicrosoftStsAuthorizationResponse) authorizationResult.getAuthorizationResponse();
+
+        // Create OAuth2Strategy using commandParameters and strategyParameters
+        final OAuth2StrategyParameters strategyParameters = new OAuth2StrategyParameters();
+        strategyParameters.setContext(parameters.getAndroidApplicationContext());
+
+        final OAuth2Strategy oAuth2Strategy = parameters
+                .getAuthority()
+                .createOAuth2Strategy(strategyParameters);
+
+        // DCF protocol step 2: Poll for token
+        TokenResult tokenResult = null;
+
+        // Create token request outside of loop so it isn't re-created after every loop
+        final MicrosoftStsTokenRequest tokenRequest = (MicrosoftStsTokenRequest) oAuth2Strategy.createTokenRequest(
+                mAuthorizationRequest,
+                authorizationResponse,
+                parameters.getAuthenticationScheme()
+        );
+
+        // Fetch wait interval
+        final int interval = Integer.parseInt(authorizationResponse.getInterval());
+
+        String errorCode = "authorization_pending";
+
+        // Loop to send multiple requests checking for token
+        while (errorCode.equals("authorization_pending")) {
+
+            // Execute Token Request
+            tokenResult = oAuth2Strategy.requestToken(tokenRequest);
+
+            if (tokenResult.getErrorResponse() != null) {
+                errorCode = tokenResult.getErrorResponse().getError();
+            }
+
+            if (errorCode.equals("authorization_pending")) {
+                // interval is passed through params
+                Thread.sleep(interval);
+            }
+        }
+
+        // Assign token result
+        acquireTokenResult.setTokenResult(tokenResult);
+
+        // Validate request success, may throw MsalServiceException
+        validateServiceResult(tokenResult);
+
+        // If the token is valid, save it into token cache
+        final List<ICacheRecord> records = saveTokens(
+                oAuth2Strategy,
+                mAuthorizationRequest,
+                acquireTokenResult.getTokenResult().getTokenResponse(),
+                parameters.getOAuth2TokenCache()
+        );
+
+        // Once the token is stored, fetch and assign the authentication result
+        final ICacheRecord newestRecord = records.get(0);
+        acquireTokenResult.setLocalAuthenticationResult(
+                new LocalAuthenticationResult(
+                        finalizeCacheRecordForResult(
+                                newestRecord,
+                                parameters.getAuthenticationScheme()
+                        ),
+                        records,
+                        SdkType.MSAL,
+                        false
+                )
+        );
+
+        logResult(TAG, tokenResult);
+
+        // End telemetry with LOCAL_DEVICE_CODE_FLOW_POLLING
+        Telemetry.emit(
+                new ApiEndEvent()
+                        .putApiId(TelemetryEventStrings.Api.LOCAL_DEVICE_CODE_FLOW_POLLING)
+        );
+
+        return acquireTokenResult;
     }
 
     /**
