44850c9673aa8229459944fc82859b38eedf3f24
shahzaibj <shahzaib.jameel@microsoft.com>
2020-07-28 16:30:22 -0700

    Add todo to fix B2C test

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/b2c/B2CIdLabSisoPolicyTest.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/b2c/B2CIdLabSisoPolicyTest.java
@@ -46,6 +46,11 @@ import org.junit.runners.Parameterized;
 import java.util.Arrays;
 import java.util.concurrent.CountDownLatch;
 
+// TODO Parameterized Tests and Rules don't work well together as the rules on the parent are
+//  executed as part of the call to the super constructor and hence the member variables in the child
+//  class aren't set yet. If we try to use such members inside a rule or during its constructor then
+//  we get an NPE. There is a plan to fix this by refactoring this test to use rule to parameterize
+//  the tests and this will be fixed very soon!
 @Ignore
 @RunWith(Parameterized.class)
 public class B2CIdLabSisoPolicyTest extends AbstractB2CTest {
