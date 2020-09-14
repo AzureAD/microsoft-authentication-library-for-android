c8706102952ce59e61f70c67509594af40806142
Brian Melton-Grace <brianmel@microsoft.com>
2020-09-14 11:12:10 -0700

    Update submodule to pickup enum -> string conversion

---
+++ msal/src/main/java/com/microsoft/identity/client/TokenShareResult.java
@@ -0,0 +1,7 @@
+package com.microsoft.identity.client;
+
+import com.microsoft.identity.common.adal.internal.tokensharing.TokenShareResultInternal;
+
+public class ITokenShareResult extends TokenShareResultInternal {
+
+}
