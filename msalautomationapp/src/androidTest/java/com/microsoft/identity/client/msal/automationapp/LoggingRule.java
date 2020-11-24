6ca68452f5e3eec85265cddcd4f76118a5945978
Shahzaib <shahzaib.jameel@microsoft.com>
2020-11-23 16:10:19 -0800

    Changes per automation logging refactor

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/LoggingRule.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/LoggingRule.java
@@ -2,8 +2,7 @@ package com.microsoft.identity.client.msal.automationapp;
 
 import com.microsoft.identity.client.ILoggerCallback;
 import com.microsoft.identity.client.Logger;
-import com.microsoft.identity.client.ui.automation.logging.FileLogStrategy;
-import com.microsoft.identity.client.ui.automation.logging.LogLevel;
+import com.microsoft.identity.client.ui.automation.logging.FileLogger;
 import com.microsoft.identity.client.ui.automation.utils.AdbShellUtils;
 
 import org.junit.rules.TestRule;
@@ -20,8 +19,8 @@ public class LoggingRule implements TestRule {
         return new Statement() {
             @Override
             public void evaluate() throws Throwable {
-                final FileLogStrategy automationLogStrategy = turnOnAutomationLogging(description);
-                final FileLogStrategy msalLogStrategy = turnOnMsalLogging(description);
+                final FileLogger automationLogStrategy = turnOnAutomationLogging(description);
+                final FileLogger msalLogStrategy = turnOnMsalLogging(description);
 
                 base.evaluate();
 
@@ -32,33 +31,43 @@ public class LoggingRule implements TestRule {
         };
     }
 
-    private FileLogStrategy turnOnMsalLogging(final Description description) {
+    private FileLogger turnOnMsalLogging(final Description description) {
         final String msalLogFileName = description.getMethodName() + "-msal.log";
-        final FileLogStrategy msalfileLogStrategy = new FileLogStrategy(msalLogFileName);
+        final FileLogger msalfileLogger = new FileLogger(msalLogFileName);
         Logger.getInstance().setLogLevel(Logger.LogLevel.VERBOSE);
         Logger.getInstance().setExternalLogger(new ILoggerCallback() {
             @Override
             public void log(String tag, Logger.LogLevel logLevel, String message, boolean containsPII) {
-                msalfileLogStrategy.log(LogLevel.VERBOSE, tag, message, null);
+                switch (logLevel) {
+                    case VERBOSE:
+                        msalfileLogger.v(tag, message);
+                    case INFO:
+                        msalfileLogger.i(tag, message);
+                    case ERROR:
+                        msalfileLogger.e(tag, message);
+                    case WARNING:
+                        msalfileLogger.w(tag, message);
+                }
             }
         });
 
-        return msalfileLogStrategy;
+        return msalfileLogger;
     }
 
-    private FileLogStrategy turnOnAutomationLogging(final Description description) {
+    private FileLogger turnOnAutomationLogging(final Description description) {
         final String automationLogFileName = description.getMethodName() + "-automation.log";
-        final FileLogStrategy automationFileLogStrategy = new FileLogStrategy(
+        final FileLogger automationFileLogger = new FileLogger(
                 automationLogFileName
         );
-        com.microsoft.identity.client.ui.automation.logging.Logger.addStrategy(
-                automationFileLogStrategy
-        );
 
-        return automationFileLogStrategy;
+        com.microsoft.identity.client.ui.automation.logging.Logger
+                .getLoggerRegistry()
+                .registerLogger(automationFileLogger);
+
+        return automationFileLogger;
     }
 
-    private void copyLogFilesToSdCard(final FileLogStrategy fileLogStrategy) throws IOException {
+    private void copyLogFilesToSdCard(final FileLogger fileLogStrategy) throws IOException {
         final String filePath = fileLogStrategy.getLogFilePath();
         final File dir = new File("/sdcard/automation");
         final File destFile = new File(dir, fileLogStrategy.getLogFile().getName());
