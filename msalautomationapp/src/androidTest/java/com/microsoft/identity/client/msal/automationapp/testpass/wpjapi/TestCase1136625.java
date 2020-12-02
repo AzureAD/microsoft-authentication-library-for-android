a24a47f054e133d733e02a6a7e7d45bf943b6b8d
krishna <kkoppart@microsoft.com>
2020-12-02 19:20:22 +0530

    Implementing Performing TLS operation

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/wpjapi/TestCase1136625.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/wpjapi/TestCase1136625.java
@@ -7,13 +7,17 @@ import androidx.test.uiautomator.UiObjectNotFoundException;
 
 import com.microsoft.identity.client.AcquireTokenParameters;
 import com.microsoft.identity.client.Prompt;
+import com.microsoft.identity.client.claims.ClaimsRequest;
+import com.microsoft.identity.client.claims.RequestedClaimAdditionalInformation;
 import com.microsoft.identity.client.msal.automationapp.R;
 import com.microsoft.identity.client.msal.automationapp.interaction.InteractiveRequest;
 import com.microsoft.identity.client.msal.automationapp.interaction.OnInteractionRequired;
 import com.microsoft.identity.client.msal.automationapp.testpass.broker.AbstractMsalBrokerTest;
 import com.microsoft.identity.client.ui.automation.broker.BrokerHost;
+import com.microsoft.identity.client.ui.automation.broker.BrokerMicrosoftAuthenticator;
 import com.microsoft.identity.client.ui.automation.broker.ITestBroker;
 import com.microsoft.identity.client.ui.automation.browser.BrowserChrome;
+import com.microsoft.identity.client.ui.automation.installer.LocalApkInstaller;
 import com.microsoft.identity.client.ui.automation.interaction.PromptHandlerParameters;
 import com.microsoft.identity.client.ui.automation.interaction.PromptParameter;
 import com.microsoft.identity.client.ui.automation.interaction.microsoftsts.AadPromptHandler;
@@ -27,7 +31,7 @@ import org.junit.Test;
 
 import java.util.concurrent.CountDownLatch;
 
-public class TestCase1136625 extends AbstractMsalBrokerTest{
+public class TestCase1136625 extends AbstractMsalBrokerTest {
 
     @Test
     public void test_1136625() throws InterruptedException, UiObjectNotFoundException {
@@ -83,35 +87,76 @@ public class TestCase1136625 extends AbstractMsalBrokerTest{
         sBroker.launch();
         sBroker.obtainDeviceId();
 
+        //installing latest version of Microsoft authenticator app.
+        ITestBroker localBrokerAuthenticator = new BrokerMicrosoftAuthenticator(new LocalApkInstaller());
+        localBrokerAuthenticator.install();
+
+        //relaunching BrokerHost.
+        sBroker.launch();
+        //do some operations on sBroker.
+
         //installing Certificate in the brokerHost.
-        UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        //UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
         UiAutomatorUtils.handleButtonClick("com.microsoft.identity.testuserapp:id/buttonInstallCert");
         UiAutomatorUtils.handleButtonClick("android:id/button1");
 
-        //perform TLS operation.//before you need some steps.
-        performTLSOpeation();
+
+        //acquiring token with claims.
+        final CountDownLatch interactiveLatch = new CountDownLatch(1);
+
+        final ClaimsRequest claimsRequest = new ClaimsRequest();
+        final RequestedClaimAdditionalInformation requestedClaimAdditionalInformation =
+                new RequestedClaimAdditionalInformation();
+
+        requestedClaimAdditionalInformation.setEssential(true);
+
+        // request the deviceid claim in ID Token
+        claimsRequest.requestClaimInIdToken("deviceid", requestedClaimAdditionalInformation);
+
+
+        final AcquireTokenParameters interactiveParameters = new AcquireTokenParameters.Builder()
+                .startAuthorizationFromActivity(mActivity)
+                .withLoginHint(null)
+                .withResource(mScopes[0])
+                .withCallback(successfulInteractiveCallback(interactiveLatch))
+                .withPrompt(Prompt.SELECT_ACCOUNT)
+                .build();
+
+        performTLSOpeation(username, password);
+
+        interactiveLatch.wait();
 
     }
 
-    private void performTLSOpeation() throws UiObjectNotFoundException {
+    private void performTLSOpeation(final String username, final String password) throws UiObjectNotFoundException {
+
         UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-        BrowserChrome chrome = new BrowserChrome();
+
+        // click on Open up from chrome tabs to open in google chrome.
         UiAutomatorUtils.handleButtonClick("com.android.chrome:id/menu_button");
         UiObject openTabs = UiAutomatorUtils.obtainUiObjectWithExactText("Open up");
         Assert.assertTrue(openTabs.exists());
         openTabs.click();
+
+        // in url removing x-client-SKU=MSAL.Android.
         UiObject urlBar = UiAutomatorUtils.obtainUiObjectWithResourceId("com.android.chrome:id/url_bar");
         Assert.assertTrue(urlBar.exists());
         String url = urlBar.getText();
+        url = url.replace("x-client-SKU=MSAL.Android", "");
 
-        //perform removing substirng from string operation.
-
-
-
+        // entering the final url in google chrome.
         UiAutomatorUtils.handleButtonClick("com.android.chrome:id/delete_button");
         urlBar.setText(url);
         device.pressEnter();
-//        i0116.
+
+        // entering credentials.
+        UiAutomatorUtils.handleInput("i0116", username);
+        UiAutomatorUtils.handleButtonClick("idSIButton9");
+
+        UiAutomatorUtils.handleInput("i0118", password);
+        UiAutomatorUtils.handleButtonClick("idSIButton9");
+
+        //installing certificate.
         UiAutomatorUtils.handleButtonClick("android:id/button1");
     }
 
