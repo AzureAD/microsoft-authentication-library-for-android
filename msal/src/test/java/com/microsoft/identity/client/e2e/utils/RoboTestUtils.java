97bf1fecfd13161e4c177109e3de782b495d2654
Adam Johnson <adjoh@microsoft.com>
2021-03-30 15:47:40 -0700

    Add info to build.gradle output, play with scheduler flush settings.

--- msal/src/test/java/com/microsoft/identity/client/e2e/utils/RoboTestUtils.java
+++ msal/src/test/java/com/microsoft/identity/client/e2e/utils/RoboTestUtils.java
@@ -2,22 +2,20 @@ package com.microsoft.identity.client.e2e.utils;
 
 import androidx.annotation.NonNull;
 
+import com.microsoft.identity.common.internal.util.ThreadUtils;
+
 import org.robolectric.RuntimeEnvironment;
 
 public class RoboTestUtils {
 
     public static void flushScheduler() {
         // wait until all runnable(s) have finished executing
+        ThreadUtils.sleepSafely(1, "RoboTestUtils:flushScheduler", "Interrupted");
         while (!RuntimeEnvironment.getMasterScheduler().advanceToLastPostedRunnable()) ;
     }
 
     public static void flushSchedulerWithDelay(@NonNull final long sleepTime) {
-        try {
-            // just wait a little for runnable(s) to enter the queue
-            Thread.sleep(sleepTime);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        ThreadUtils.sleepSafely((int) sleepTime, "RoboTestUtils:flushSchedulerWithDelay", "Interrupted");
 
         // if there are no runnable(s) after the delay, then we can just return
         if (RuntimeEnvironment.getMasterScheduler().size() == 0) {
