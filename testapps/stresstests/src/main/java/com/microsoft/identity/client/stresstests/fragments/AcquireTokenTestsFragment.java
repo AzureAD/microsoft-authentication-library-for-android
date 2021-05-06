734f09cd2345765ba35cd4c5b16c767b479a3550
Moses Gitau <gitaumoses4@gmail.com>
2021-05-06 11:21:43 +0300

    Create the stress testing application

---
+++ testapps/stresstests/src/main/java/com/microsoft/identity/client/stresstests/fragments/AcquireTokenTestsFragment.java
@@ -0,0 +1,10 @@
+package com.microsoft.identity.client.stresstests.fragments;
+
+import com.microsoft.identity.client.stresstests.R;
+
+public class AcquireTokenTestsFragment extends BaseFragment{
+    @Override
+    public int getLayoutResource() {
+        return R.layout.fragment_acquire_token_tests;
+    }
+}
