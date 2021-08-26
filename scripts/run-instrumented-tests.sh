84dd33c256a41f67b95459779a347f8321de4b8f
p3dr0rv <76129899+p3dr0rv@users.noreply.github.com>
2021-08-26 11:56:30 -0700

    add cold boot

--- scripts/run-instrumented-tests.sh
+++ scripts/run-instrumented-tests.sh
@@ -3,7 +3,7 @@ echo =============================================
 echo Starting ADB Daemon
 echo =============================================
 adb start-server
-emulator @test -no-window -no-audio -wipe-data &
+emulator @test -no-window -no-audio -no-snapshot -wipe-data &
 sleep 30
 gradle -version
 echo =============================================
