9d818bda051b69c6067d9c55306a97886142aee6
Moses Gitau <gitaumoses4@gmail.com>
2021-09-29 09:10:41 +0300

    Use network automation rule

---
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/network/BaseMsalUiNetworkTest.java
@@ -0,0 +1,127 @@
+//  Copyright (c) Microsoft Corporation.
+//  All rights reserved.
+//
+//  This code is licensed under the MIT License.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files(the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions :
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+package com.microsoft.identity.client.msal.automationapp.testpass.network;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import com.microsoft.identity.client.AuthenticationCallback;
+import com.microsoft.identity.client.IAuthenticationResult;
+import com.microsoft.identity.client.SilentAuthenticationCallback;
+import com.microsoft.identity.client.exception.MsalException;
+import com.microsoft.identity.client.msal.automationapp.AbstractMsalUiTest;
+import com.microsoft.identity.client.msal.automationapp.R;
+import com.microsoft.identity.client.ui.automation.network.NetworkTestStateManager;
+import com.microsoft.identity.client.ui.automation.rules.NetworkTestRule;
+import com.microsoft.identity.internal.testutils.labutils.LabConstants;
+import com.microsoft.identity.internal.testutils.labutils.LabUserQuery;
+
+import org.junit.Rule;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Sets up a base class for running MSAL network tests.
+ */
+public abstract class BaseMsalUiNetworkTest extends AbstractMsalUiTest {
+
+    @Rule(order = 3)
+    public NetworkTestRule<IAuthenticationResult> networkTestRule = new NetworkTestRule<>();
+
+    @Override
+    public void setup() {
+        mContext = ApplicationProvider.getApplicationContext();
+
+        // Turn WIFI ON before running the first test.
+        NetworkTestStateManager.resetNetworkState(mContext);
+
+        super.setup();
+    }
+
+    @Override
+    protected AuthenticationCallback successfulInteractiveCallback(CountDownLatch latch) {
+        return new AuthenticationCallback() {
+            @Override
+            public void onCancel() {
+                latch.countDown();
+                networkTestRule.setException(new Exception("Interactive flow cancelled by user."));
+            }
+
+            @Override
+            public void onSuccess(IAuthenticationResult authenticationResult) {
+                latch.countDown();
+                networkTestRule.setResult(authenticationResult);
+            }
+
+            @Override
+            public void onError(MsalException exception) {
+                latch.countDown();
+                networkTestRule.setException(exception);
+            }
+        };
+    }
+
+    @Override
+    protected SilentAuthenticationCallback successfulSilentCallback(CountDownLatch latch) {
+        return new SilentAuthenticationCallback() {
+            @Override
+            public void onSuccess(IAuthenticationResult authenticationResult) {
+                latch.countDown();
+                networkTestRule.setResult(authenticationResult);
+            }
+
+            @Override
+            public void onError(MsalException exception) {
+                latch.countDown();
+                networkTestRule.setException(exception);
+            }
+        };
+    }
+
+    @Override
+    public LabUserQuery getLabUserQuery() {
+        final LabUserQuery query = new LabUserQuery();
+        query.azureEnvironment = LabConstants.AzureEnvironment.AZURE_CLOUD;
+        return query;
+    }
+
+    @Override
+    public String getTempUserType() {
+        return null;
+    }
+
+    @Override
+    public String[] getScopes() {
+        return new String[]{"User.read"};
+    }
+
+    @Override
+    public String getAuthority() {
+        return mApplication.getConfiguration().getDefaultAuthority().toString();
+    }
+
+    @Override
+    public int getConfigFileResourceId() {
+        return R.raw.msal_config_webview;
+    }
+
+}
--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/network/TestCaseNetwork.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/network/TestCaseNetwork.java
@@ -1,39 +1,80 @@
 package com.microsoft.identity.client.msal.automationapp.testpass.network;
 
+import android.util.Log;
+
 import com.microsoft.identity.client.AcquireTokenParameters;
 import com.microsoft.identity.client.AcquireTokenSilentParameters;
 import com.microsoft.identity.client.IAccount;
-import com.microsoft.identity.client.IAuthenticationResult;
 import com.microsoft.identity.client.Prompt;
-import com.microsoft.identity.client.SilentAuthenticationCallback;
-import com.microsoft.identity.client.exception.MsalException;
 import com.microsoft.identity.client.msal.automationapp.interaction.InteractiveRequest;
 import com.microsoft.identity.client.msal.automationapp.interaction.OnInteractionRequired;
 import com.microsoft.identity.client.ui.automation.TokenRequestLatch;
 import com.microsoft.identity.client.ui.automation.TokenRequestTimeout;
+import com.microsoft.identity.client.ui.automation.annotations.NetworkStatesFile;
+import com.microsoft.identity.client.ui.automation.annotations.NetworkTestTimeout;
 import com.microsoft.identity.client.ui.automation.interaction.PromptHandlerParameters;
 import com.microsoft.identity.client.ui.automation.interaction.PromptParameter;
 import com.microsoft.identity.client.ui.automation.interaction.microsoftsts.AadPromptHandler;
-import com.microsoft.identity.client.ui.automation.network.NetworkTestStateManager;
-import com.microsoft.identity.client.ui.automation.sdk.ResultFuture;
+import com.microsoft.identity.client.ui.automation.rules.NetworkTestRule;
 import com.microsoft.identity.internal.testutils.labutils.LabConfig;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.Rule;
+import org.junit.Test;
 
-import java.io.IOException;
 import java.util.Arrays;
 
-@RunWith(Parameterized.class)
-public class TestNetworkAcquireTokenSilent extends AbstractMsalUiNetworkTest<IAccount> {
+public class TestCaseNetwork extends BaseMsalUiNetworkTest {
 
-    @Parameterized.Parameters(name = "acquireTokenSilent_{1}")
-    public static Iterable<Object[]> parameters() throws IOException, ClassNotFoundException {
-        return generateTestParameters("input_acquireTokenSilentlyWithoutBroker.csv", "output_acquireTokenSilentlyWithoutBroker.csv");
+    @NetworkStatesFile("input_acquireTokenWithoutBroker.csv")
+    @NetworkTestTimeout(seconds = 120)
+    @Test
+    public void test_acquireTokenWithoutBroker() {
+        Log.d("NetworkTestRule", "Running test....");
+        TokenRequestLatch tokenRequestLatch = new TokenRequestLatch(1);
+        final AcquireTokenParameters parameters = new AcquireTokenParameters.Builder()
+                .startAuthorizationFromActivity(mActivity)
+                .withLoginHint(mLoginHint)
+                .withScopes(Arrays.asList(mScopes))
+                .withCallback(successfulInteractiveCallback(tokenRequestLatch))
+                .withPrompt(Prompt.SELECT_ACCOUNT)
+                .build();
+
+
+        final InteractiveRequest interactiveRequest = new InteractiveRequest(
+                mApplication,
+                parameters,
+                new OnInteractionRequired() {
+                    @Override
+                    public void handleUserInteraction() {
+                        final String username = mLoginHint;
+                        final String password = LabConfig.getCurrentLabConfig().getLabUserPassword();
+
+                        final PromptHandlerParameters promptHandlerParameters = PromptHandlerParameters.builder()
+                                .prompt(PromptParameter.SELECT_ACCOUNT)
+                                .loginHint(mLoginHint)
+                                .sessionExpected(false)
+                                .consentPageExpected(false)
+                                .speedBumpExpected(false)
+                                .build();
+
+                        new AadPromptHandler(promptHandlerParameters)
+                                .handlePrompt(username, password);
+                    }
+                }
+        );
+
+        interactiveRequest.execute();
+
+        tokenRequestLatch.await(TokenRequestTimeout.SHORT);
+
+        mBrowser.clear();
+        mAccount = null;
     }
 
-    @Override
-    public IAccount prepare() {
+    @NetworkStatesFile("input_acquireTokenSilentWithoutBroker.csv")
+    @NetworkTestTimeout(seconds = 120)
+    @Test
+    public void test_acquireTokenSilentWithoutBroker() {
         final TokenRequestLatch latch = new TokenRequestLatch(1);
 
         final AcquireTokenParameters parameters = new AcquireTokenParameters.Builder()
@@ -71,32 +112,23 @@ public class TestNetworkAcquireTokenSilent extends AbstractMsalUiNetworkTest<IAc
         interactiveRequest.execute();
         latch.await(TokenRequestTimeout.SHORT);
 
-        return getAccount();
-    }
+        IAccount account = getAccount();
 
-    @Override
-    public void execute(IAccount account, ResultFuture<String, Exception> resultFuture) {
         final int numberOfTests = 10;
         for (int i = 0; i < numberOfTests; i++) {
+            TokenRequestLatch tokenRequestLatch = new TokenRequestLatch(1);
+
             final AcquireTokenSilentParameters silentParameters = new AcquireTokenSilentParameters.Builder()
                     .forAccount(account)
                     .fromAuthority(account.getAuthority())
                     .forceRefresh(true)
                     .withScopes(Arrays.asList(mScopes))
-                    .withCallback(new SilentAuthenticationCallback() {
-                        @Override
-                        public void onSuccess(IAuthenticationResult authenticationResult) {
-                            resultFuture.setResult(authenticationResult.getAccessToken());
-                        }
-
-                        @Override
-                        public void onError(MsalException exception) {
-                            resultFuture.setException(exception);
-                        }
-                    })
+                    .withCallback(successfulSilentCallback(tokenRequestLatch))
                     .build();
 
             mApplication.acquireTokenSilentAsync(silentParameters);
+
+            tokenRequestLatch.await(TokenRequestTimeout.SILENT);
         }
     }
 }
