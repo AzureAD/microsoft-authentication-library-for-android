c90aedd6ad5688ca6d8b466196602c1e03f20f06
Paul <paulkagirim@gmail.com>
2021-12-08 19:35:11 +0300

    claims test

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/claims/ClaimsRequestTest.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/claims/ClaimsRequestTest.java
@@ -3,69 +3,78 @@ package com.microsoft.identity.client.msal.automationapp.testpass.claims;
 import com.microsoft.identity.client.Prompt;
 import com.microsoft.identity.client.claims.ClaimsRequest;
 import com.microsoft.identity.client.claims.RequestedClaimAdditionalInformation;
-import com.microsoft.identity.client.msal.automationapp.AbstractMsalUiTest;
 import com.microsoft.identity.client.msal.automationapp.R;
 import com.microsoft.identity.client.msal.automationapp.sdk.MsalAuthResult;
 import com.microsoft.identity.client.msal.automationapp.sdk.MsalAuthTestParams;
 import com.microsoft.identity.client.msal.automationapp.sdk.MsalSdk;
+import com.microsoft.identity.client.msal.automationapp.testpass.broker.AbstractMsalBrokerTest;
 import com.microsoft.identity.client.ui.automation.TokenRequestTimeout;
+import com.microsoft.identity.client.ui.automation.interaction.OnInteractionRequired;
+import com.microsoft.identity.client.ui.automation.interaction.PromptHandlerParameters;
 import com.microsoft.identity.client.ui.automation.interaction.PromptParameter;
 import com.microsoft.identity.client.ui.automation.interaction.microsoftsts.AadPromptHandler;
-import com.microsoft.identity.client.ui.automation.interaction.microsoftsts.MicrosoftStsPromptHandlerParameters;
 import com.microsoft.identity.internal.testutils.labutils.LabConfig;
 import com.microsoft.identity.internal.testutils.labutils.LabConstants;
 import com.microsoft.identity.internal.testutils.labutils.LabUserQuery;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Objects;
 
-public class ClaimsRequestTest extends AbstractMsalUiTest {
+public class ClaimsRequestTest extends AbstractMsalBrokerTest {
 
     @Test
     public void testAcquireTokenSilentlyWithClaims() throws Throwable {
-        ClaimsRequest claimsRequest = new ClaimsRequest();
-        RequestedClaimAdditionalInformation additionalInformation = new RequestedClaimAdditionalInformation();
-        additionalInformation.setEssential(true);
-        additionalInformation.setValue("650d9495-66a1-4f6b-bcd7-4f0add50c81a");
-        claimsRequest.requestClaimInAccessToken("device_id", additionalInformation);
-
         final String username = mLoginHint;
         final String password = LabConfig.getCurrentLabConfig().getLabUserPassword();
+        final String requestedClaim = "deviceid";
 
-        final MsalSdk msalSdk = new MsalSdk();
+        final ClaimsRequest claimsRequest = new ClaimsRequest();
+        final RequestedClaimAdditionalInformation requestedClaimAdditionalInformation = new RequestedClaimAdditionalInformation();
+        requestedClaimAdditionalInformation.setEssential(true);
+        claimsRequest.requestClaimInIdToken(requestedClaim, requestedClaimAdditionalInformation);
+
         final MsalAuthTestParams authTestParams = MsalAuthTestParams.builder()
-                .loginHint(mLoginHint)
                 .activity(mActivity)
+                .loginHint(username)
+                .claims(claimsRequest)
                 .scopes(Arrays.asList(mScopes))
                 .promptParameter(Prompt.SELECT_ACCOUNT)
                 .msalConfigResourceId(getConfigFileResourceId())
-                .claims(claimsRequest)
                 .build();
 
-        final MsalAuthResult authResult = msalSdk.acquireTokenInteractive(authTestParams, new com.microsoft.identity.client.ui.automation.interaction.OnInteractionRequired() {
+        final MsalSdk msalSdk = new MsalSdk();
+        final MsalAuthResult authResult = msalSdk.acquireTokenInteractive(authTestParams, new OnInteractionRequired() {
             @Override
             public void handleUserInteraction() {
-                final MicrosoftStsPromptHandlerParameters promptHandlerParameters = MicrosoftStsPromptHandlerParameters.builder()
+                final PromptHandlerParameters promptHandlerParameters = PromptHandlerParameters.builder()
                         .prompt(PromptParameter.SELECT_ACCOUNT)
-                        .loginHint(mLoginHint)
+                        .loginHint(username)
                         .sessionExpected(false)
                         .consentPageExpected(false)
                         .speedBumpExpected(false)
+                        .broker(mBroker)
+                        .expectingBrokerAccountChooserActivity(false)
+                        .expectingLoginPageAccountPicker(false)
+                        .registerPageExpected(true)
                         .build();
 
                 new AadPromptHandler(promptHandlerParameters)
                         .handlePrompt(username, password);
             }
-        }, TokenRequestTimeout.MEDIUM);
+        }, TokenRequestTimeout.LONG);
 
         authResult.assertSuccess();
+        Assert.assertTrue(Objects.requireNonNull(msalSdk.getAccount(mActivity, getConfigFileResourceId(), username).getClaims()).containsKey("deviceid"));
     }
 
     @Override
     public LabUserQuery getLabUserQuery() {
         final LabUserQuery query = new LabUserQuery();
-        query.azureEnvironment = LabConstants.AzureEnvironment.AZURE_CLOUD;
+        query.userType = LabConstants.UserType.CLOUD;
+        query.azureEnvironment = LabConstants.AzureEnvironment.AZURE_US_GOVERNMENT;
         return query;
     }
 
@@ -86,6 +95,6 @@ public class ClaimsRequestTest extends AbstractMsalUiTest {
 
     @Override
     public int getConfigFileResourceId() {
-        return R.raw.msal_config_webview;
+        return R.raw.msal_config_instance_aware_common;
     }
 }
