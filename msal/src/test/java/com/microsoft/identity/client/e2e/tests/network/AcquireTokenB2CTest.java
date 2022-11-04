63adb484b5fbfa3023dcef821a43a340b014b7fa
Shahzaib <shahzaib.jameel@microsoft.com>
2022-11-03 19:17:21 -0700

    Comment out B2C tests as not applicable to slice validation

--- msal/src/test/java/com/microsoft/identity/client/e2e/tests/network/AcquireTokenB2CTest.java
+++ msal/src/test/java/com/microsoft/identity/client/e2e/tests/network/AcquireTokenB2CTest.java
@@ -20,64 +20,64 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-package com.microsoft.identity.client.e2e.tests.network;
-
-import com.microsoft.identity.internal.testutils.labutils.LabConstants;
-import com.microsoft.identity.internal.testutils.labutils.LabUserQuery;
-
-import static com.microsoft.identity.internal.testutils.TestConstants.Configurations.B2C_CUSTOM_DOMAIN_CONFIG_FILE_PATH;
-import static com.microsoft.identity.internal.testutils.TestConstants.Configurations.B2C_GLOBAL_DOMAIN_CONFIG_FILE_PATH;
-import static com.microsoft.identity.internal.testutils.TestConstants.Scopes.B2C_READ_SCOPE;
-
-/**
- * Run all tests in the {@link AcquireTokenNetworkTest} class using B2C
- */
-public abstract class AcquireTokenB2CTest extends AcquireTokenNetworkTest {
-
-    @Override
-    public String getAuthority() {
-        // TODO: We need to refactor this to get the authority from account once we fix the
-        //  getAuthority logic for the case of B2C. For details see {@link Account#getAuthority()}
-        return mApplication.getConfiguration().getDefaultAuthority().getAuthorityURL().toString();
-    }
-
-    @Override
-    public String[] getScopes() {
-        return B2C_READ_SCOPE;
-    }
-
-    public static class B2CLocalUserGlobalMsftDomain extends AcquireTokenB2CTest {
-
-        @Override
-        public String getConfigFilePath() {
-            return B2C_GLOBAL_DOMAIN_CONFIG_FILE_PATH;
-        }
-
-        @Override
-        public LabUserQuery getLabUserQuery() {
-            final LabUserQuery query = new LabUserQuery();
-            query.userType = LabConstants.UserType.B2C;
-            query.b2cProvider = LabConstants.B2CProvider.LOCAL;
-            return query;
-        }
-
-    }
-
-    public static class B2CLocalUserCustomDomain extends AcquireTokenB2CTest {
-
-        @Override
-        public String getConfigFilePath() {
-            return B2C_CUSTOM_DOMAIN_CONFIG_FILE_PATH;
-        }
-
-        @Override
-        public LabUserQuery getLabUserQuery() {
-            final LabUserQuery query = new LabUserQuery();
-            query.userType = LabConstants.UserType.B2C;
-            query.b2cProvider = LabConstants.B2CProvider.LOCAL;
-            return query;
-        }
-
-    }
-
-}
+//package com.microsoft.identity.client.e2e.tests.network;
+//
+//import com.microsoft.identity.internal.testutils.labutils.LabConstants;
+//import com.microsoft.identity.internal.testutils.labutils.LabUserQuery;
+//
+//import static com.microsoft.identity.internal.testutils.TestConstants.Configurations.B2C_CUSTOM_DOMAIN_CONFIG_FILE_PATH;
+//import static com.microsoft.identity.internal.testutils.TestConstants.Configurations.B2C_GLOBAL_DOMAIN_CONFIG_FILE_PATH;
+//import static com.microsoft.identity.internal.testutils.TestConstants.Scopes.B2C_READ_SCOPE;
+//
+///**
+// * Run all tests in the {@link AcquireTokenNetworkTest} class using B2C
+// */
+//public abstract class AcquireTokenB2CTest extends AcquireTokenNetworkTest {
+//
+//    @Override
+//    public String getAuthority() {
+//        // TODO: We need to refactor this to get the authority from account once we fix the
+//        //  getAuthority logic for the case of B2C. For details see {@link Account#getAuthority()}
+//        return mApplication.getConfiguration().getDefaultAuthority().getAuthorityURL().toString();
+//    }
+//
+//    @Override
+//    public String[] getScopes() {
+//        return B2C_READ_SCOPE;
+//    }
+//
+//    public static class B2CLocalUserGlobalMsftDomain extends AcquireTokenB2CTest {
+//
+//        @Override
+//        public String getConfigFilePath() {
+//            return B2C_GLOBAL_DOMAIN_CONFIG_FILE_PATH;
+//        }
+//
+//        @Override
+//        public LabUserQuery getLabUserQuery() {
+//            final LabUserQuery query = new LabUserQuery();
+//            query.userType = LabConstants.UserType.B2C;
+//            query.b2cProvider = LabConstants.B2CProvider.LOCAL;
+//            return query;
+//        }
+//
+//    }
+//
+//    public static class B2CLocalUserCustomDomain extends AcquireTokenB2CTest {
+//
+//        @Override
+//        public String getConfigFilePath() {
+//            return B2C_CUSTOM_DOMAIN_CONFIG_FILE_PATH;
+//        }
+//
+//        @Override
+//        public LabUserQuery getLabUserQuery() {
+//            final LabUserQuery query = new LabUserQuery();
+//            query.userType = LabConstants.UserType.B2C;
+//            query.b2cProvider = LabConstants.B2CProvider.LOCAL;
+//            return query;
+//        }
+//
+//    }
+//
+//}
