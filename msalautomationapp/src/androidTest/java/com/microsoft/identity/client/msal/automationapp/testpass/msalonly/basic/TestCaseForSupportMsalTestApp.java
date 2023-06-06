b24b4e16ac0d10ffc7c1981a1f9cdfe5b114c744
yixilin <yixilin@microsoft.com>
2023-06-06 09:01:38 +0800

    add test case for msal test app testing methods acquireToken acquireTokenSilent and getUsers

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/msalonly/basic/TestCaseForSupportMsalTestApp.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/msalonly/basic/TestCaseForSupportMsalTestApp.java
@@ -1,39 +1,35 @@
 package com.microsoft.identity.client.msal.automationapp.testpass.msalonly.basic;
 
+import android.util.Log;
+
+import com.microsoft.identity.client.msal.automationapp.AbstractMsalUiTest;
 import com.microsoft.identity.client.msal.automationapp.R;
 import com.microsoft.identity.client.msal.automationapp.testpass.broker.AbstractMsalBrokerTest;
 import com.microsoft.identity.client.ui.automation.annotations.RetryOnFailure;
 import com.microsoft.identity.client.ui.automation.annotations.RunOnAPI29Minus;
 import com.microsoft.identity.client.ui.automation.annotations.SupportedBrokers;
 import com.microsoft.identity.client.ui.automation.app.MsalTestApp;
-import com.microsoft.identity.client.ui.automation.broker.BrokerLTW;
-import com.microsoft.identity.client.ui.automation.interaction.PromptHandlerParameters;
+import com.microsoft.identity.client.ui.automation.broker.BrokerMicrosoftAuthenticator;
 import com.microsoft.identity.client.ui.automation.interaction.PromptParameter;
+import com.microsoft.identity.client.ui.automation.interaction.UiResponse;
+import com.microsoft.identity.client.ui.automation.interaction.microsoftsts.MicrosoftStsPromptHandlerParameters;
 import com.microsoft.identity.labapi.utilities.client.LabQuery;
 import com.microsoft.identity.labapi.utilities.constants.TempUserType;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
-@RetryOnFailure
+@RetryOnFailure(retryCount = 2)
 @RunOnAPI29Minus
-@SupportedBrokers(brokers = {BrokerLTW.class})
+@SupportedBrokers(brokers = {BrokerMicrosoftAuthenticator.class})
 public class TestCaseForSupportMsalTestApp extends AbstractMsalBrokerTest {
 
     @Test
     public void TestCaseForSupportMsalTestApp () throws Throwable {
         final String username = mLabAccount.getUsername();
         final String password = mLabAccount.getPassword();
-
-        // test for installing msal test app crash error
-//        UiDevice mDevice = UiDevice.getInstance(getInstrumentation());
-//        String b = mDevice.executeShellCommand("ls /");
-//        Log.d("TestCaseForSupportMsalTestApp", "installPackage: " + b);
-//        String a = mDevice.executeShellCommand("pm install -t /data/local/tmp/MsalTestApp.apk");
-//        Log.d("TestCaseForSupportMsalTestApp", "installPackage: " + a);
 
         MsalTestApp msalTestApp = new MsalTestApp();
         msalTestApp.uninstall();
@@ -42,7 +38,7 @@ public class TestCaseForSupportMsalTestApp extends AbstractMsalBrokerTest {
         msalTestApp.handleFirstRun();
 
         // acquire toke interactively and validate the token
-        final PromptHandlerParameters promptHandlerParameters = PromptHandlerParameters.builder()
+        final MicrosoftStsPromptHandlerParameters promptHandlerParameters = MicrosoftStsPromptHandlerParameters.builder()
                 .prompt(PromptParameter.SELECT_ACCOUNT)
                 .loginHint(username)
                 .sessionExpected(false)
@@ -62,16 +58,44 @@ public class TestCaseForSupportMsalTestApp extends AbstractMsalBrokerTest {
                 .build();
 
         String token = msalTestApp.acquireToken(username, password, promptHandlerParameters);
+        Log.d("TestCaseForSupportMsalTestApp", "TestCaseForSupportMsalTestApp: tokenInteractive = " + token);
 
-        // Small wait to allow getting token to complete
-        Thread.sleep(TimeUnit.SECONDS.toMillis(5));
-
-        Map<String, ?> validatedToken = msalTestApp.validateToken(token);
+//        Map<String, ?> validatedToken = msalTestApp.validateToken(token);
 //        Assert.assertTrue(validatedToken != null);
+
         // then acquire token silently and validate the token
+        msalTestApp.forceStop();
+        msalTestApp.launch();
+        msalTestApp.handleFirstRun();
+        final MicrosoftStsPromptHandlerParameters promptHandlerParametersForSilentAcquireToken = MicrosoftStsPromptHandlerParameters.builder()
+                .prompt(PromptParameter.SELECT_ACCOUNT)
+                .loginHint(null)
+                .sessionExpected(false)
+                .broker(null)
+                .expectingBrokerAccountChooserActivity(false)
+                .expectingProvidedAccountInBroker(false)
+                .expectingLoginPageAccountPicker(false)
+                .expectingProvidedAccountInCookie(false)
+                .consentPageExpected(false)
+                .passwordPageExpected(false)
+                .speedBumpExpected(false)
+                .registerPageExpected(false)
+                .enrollPageExpected(false)
+                .staySignedInPageExpected(false)
+                .verifyYourIdentityPageExpected(false)
+                .howWouldYouLikeToSignInExpected(false)
+                .build();
+        String silentToken = msalTestApp.acquireTokenSilent(username, password, promptHandlerParametersForSilentAcquireToken);
+        Log.d("TestCaseForSupportMsalTestApp", "TestCaseForSupportMsalTestApp: tokenSilent = " + silentToken);
+        Assert.assertTrue(silentToken != null);
 
         // finally get users and validate the users
-
+        msalTestApp.forceStop();
+        msalTestApp.launch();
+        msalTestApp.handleFirstRun();
+        List<String> users = msalTestApp.getUsers();
+        Log.d("TestCaseForSupportMsalTestApp", "TestCaseForSupportMsalTestApp: userList = " + users);
+        Assert.assertTrue(users.size() == 1);
     }
 
     // if getLabQuery return null then will use getTempUserType to create account
