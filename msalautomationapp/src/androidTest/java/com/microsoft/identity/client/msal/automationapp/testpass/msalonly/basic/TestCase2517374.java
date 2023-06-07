2a4876a7876a7240b5bbe0510c4f97b71a9191b8
yixilin <yixilin@microsoft.com>
2023-06-07 23:44:40 +0800

    remove redundant params

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/msalonly/basic/TestCase2517374.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/msalonly/basic/TestCase2517374.java
@@ -9,22 +9,14 @@ import com.microsoft.identity.client.ui.automation.annotations.RunOnAPI29Minus;
 import com.microsoft.identity.client.ui.automation.annotations.SupportedBrokers;
 import com.microsoft.identity.client.ui.automation.app.MsalTestApp;
 import com.microsoft.identity.client.ui.automation.broker.BrokerCompanyPortal;
-import com.microsoft.identity.client.ui.automation.broker.BrokerHost;
-import com.microsoft.identity.client.ui.automation.broker.BrokerLTW;
-import com.microsoft.identity.client.ui.automation.broker.BrokerMicrosoftAuthenticator;
 import com.microsoft.identity.client.ui.automation.interaction.PromptParameter;
 import com.microsoft.identity.client.ui.automation.interaction.microsoftsts.MicrosoftStsPromptHandlerParameters;
-import com.microsoft.identity.common.java.providers.oauth2.IDToken;
 import com.microsoft.identity.labapi.utilities.client.LabQuery;
 import com.microsoft.identity.labapi.utilities.constants.TempUserType;
-import com.microsoft.identity.labapi.utilities.jwt.JWTParserFactory;
-
-import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.util.List;
-import java.util.Map;
+
 
 @RetryOnFailure(retryCount = 2)
 @RunOnAPI29Minus
@@ -69,25 +61,7 @@ public class TestCase2517374 extends AbstractMsalBrokerTest {
         msalTestApp.forceStop();
         msalTestApp.launch();
         msalTestApp.handleFirstRun();
-        final MicrosoftStsPromptHandlerParameters promptHandlerParametersForSilentAcquireToken = MicrosoftStsPromptHandlerParameters.builder()
-                .prompt(PromptParameter.SELECT_ACCOUNT)
-                .loginHint(null)
-                .sessionExpected(false)
-                .broker(mBroker)
-                .expectingBrokerAccountChooserActivity(false)
-                .expectingProvidedAccountInBroker(false)
-                .expectingLoginPageAccountPicker(false)
-                .expectingProvidedAccountInCookie(false)
-                .consentPageExpected(false)
-                .passwordPageExpected(false)
-                .speedBumpExpected(false)
-                .registerPageExpected(false)
-                .enrollPageExpected(false)
-                .staySignedInPageExpected(false)
-                .verifyYourIdentityPageExpected(false)
-                .howWouldYouLikeToSignInExpected(false)
-                .build();
-        String silentToken = msalTestApp.acquireTokenSilent(username, password, promptHandlerParametersForSilentAcquireToken);
+        String silentToken = msalTestApp.acquireTokenSilent();
         Assert.assertNotNull(silentToken);
 
         // finally get users and validate the users
