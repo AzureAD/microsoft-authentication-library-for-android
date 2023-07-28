a277dfa4821b2c8cbb82649d2e2d779b21da8ecb
yixilin <yixilin@microsoft.com>
2023-07-28 11:38:02 +0800

    add layout and method for showDialog

--- testapps/testapp/src/main/java/com/microsoft/identity/client/testapp/AcquireTokenFragment.java
+++ testapps/testapp/src/main/java/com/microsoft/identity/client/testapp/AcquireTokenFragment.java
@@ -25,6 +25,7 @@ package com.microsoft.identity.client.testapp;
 import static com.microsoft.identity.client.testapp.R.id.enablePII;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
@@ -337,7 +338,7 @@ public class AcquireTokenFragment extends Fragment {
             public void onClick(View v) {
                 final String activeBrokerPkgName = mMsalWrapper.getActiveBrokerPkgName(activity);
                 final String activeBrokerPkgNameMsg = StringUtil.isNullOrEmpty(activeBrokerPkgName) ? "Could not find a valid broker" : "Active broker pkg name : " + activeBrokerPkgName;
-                AcquireTokenFragment.this.showMessage(activeBrokerPkgNameMsg);
+                AcquireTokenFragment.this.showDialog(activeBrokerPkgNameMsg);
             }
         });
 
@@ -363,7 +364,7 @@ public class AcquireTokenFragment extends Fragment {
 
                             @Override
                             public void showMessage(String message) {
-                                AcquireTokenFragment.this.showMessage(message);
+                                AcquireTokenFragment.this.showDialog(message);
                             }
                         });
             }
@@ -603,11 +604,27 @@ public class AcquireTokenFragment extends Fragment {
 
     private void showMessage(final String msg) {
         new Handler(getActivity().getMainLooper()).post(new Runnable() {
-
             @Override
             public void run() {
                 Toast.makeText(getContext(), msg, Toast.LENGTH_LONG).show();
                 mStatus.setText(msg);
+            }
+        });
+    }
+
+    private void showDialog(final String msg) {
+        new Handler(getActivity().getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.custom_dialog_layout, null);
+                builder.setView(dialogView);
+
+                TextView dialogMessage = dialogView.findViewById(R.id.dialog_message);
+                dialogMessage.setText(msg);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
     }
