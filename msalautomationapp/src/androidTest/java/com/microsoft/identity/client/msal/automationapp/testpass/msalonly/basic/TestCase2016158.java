657325a4a7f4638c1f8ac499c7376c83bae45a12
Fadi Durah <fadidurah@microsoft.com>
2023-07-31 17:53:50 -0400

    fix tests

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/msalonly/basic/TestCase2016158.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/msalonly/basic/TestCase2016158.java
@@ -47,7 +47,7 @@ import java.util.Arrays;
 // [MSAL-Only] A single-tenant app makes a silent request with common authority. It should fail..
 // https://identitydivision.visualstudio.com/DevEx/_workitems/edit/2016158
 @RunOnAPI29Minus("Consent Page")
-@RetryOnFailure
+@RetryOnFailure(retryCount = 2)
 public class TestCase2016158 extends AbstractMsalUiTest {
 
     @Test
