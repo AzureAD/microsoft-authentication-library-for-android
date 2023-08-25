4fdde5c3ea81e521f399ada53bfbd54e55d578b0
Yixi Lin <yixilin@microsoft.com>
2023-08-25 23:14:24 +0800

    resolve comments

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2582288.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2582288.java
@@ -45,7 +45,6 @@ import org.junit.Test;
 @RunOnAPI29Minus("OneAuth test app dependency")
 @SupportedBrokers(brokers = {BrokerLTW.class})
 public class TestCase2582288 extends AbstractMsalBrokerTest {
-
     @Test
     public void test_2582288() throws Throwable {
         final String username = mLabAccount.getUsername();
@@ -90,9 +89,7 @@ public class TestCase2582288 extends AbstractMsalBrokerTest {
 
         // Click on getAccessToken
         // User should not be prompted for credentials and token is returned
-        final String tokenOneAuth = oneAuthTestApp.acquireTokenSilent();
-        Assert.assertNotNull(tokenOneAuth);
-        Assert.assertNotEquals("", tokenOneAuth);
+        oneAuthTestApp.handleSignInWithoutPrompt();
     }
 
     @Override
