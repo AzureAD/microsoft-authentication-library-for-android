7b8567b4609027cf7a780817cfbe75cc49760c27
Yixi Lin <yixilin@microsoft.com>
2023-09-13 12:33:49 +0800

    add supported broker

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2584411.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2584411.java
@@ -26,6 +26,7 @@ import com.microsoft.identity.client.msal.automationapp.R;
 import com.microsoft.identity.client.msal.automationapp.testpass.broker.AbstractMsalBrokerTest;
 import com.microsoft.identity.client.ui.automation.annotations.LTWTests;
 import com.microsoft.identity.client.ui.automation.annotations.RunOnAPI29Minus;
+import com.microsoft.identity.client.ui.automation.annotations.SupportedBrokers;
 import com.microsoft.identity.client.ui.automation.app.MsalTestApp;
 import com.microsoft.identity.client.ui.automation.broker.BrokerCompanyPortal;
 import com.microsoft.identity.client.ui.automation.broker.BrokerLTW;
@@ -42,6 +43,7 @@ import org.junit.Test;
 // https://identitydivision.visualstudio.com/Engineering/_workitems/edit/2584411
 @LTWTests
 @RunOnAPI29Minus
+@SupportedBrokers(brokers = {BrokerMicrosoftAuthenticator.class})
 public class TestCase2584411 extends AbstractMsalBrokerTest {
     @Test
     public void test_2584411() throws Throwable {
