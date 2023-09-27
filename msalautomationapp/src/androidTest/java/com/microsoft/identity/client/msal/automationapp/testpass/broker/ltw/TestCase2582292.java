65911f985cf37b85bc88b7e19387b7a75cff6cd8
Sowmya Malayanur <69237821+somalaya@users.noreply.github.com>
2023-09-26 20:51:14 -0700

    Update TestCase2582292.java

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2582292.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2582292.java
@@ -50,7 +50,6 @@ import java.util.List;
 
 // If LTW is the active broker, and request is made through Authenticator from an old MSAL in shared device mode, nothing should break
 // https://identitydivision.visualstudio.com/Engineering/_workitems/edit/2582292
-@LocalBrokerHostDebugUiTest
 @SupportedBrokers(brokers = {BrokerMicrosoftAuthenticator.class})
 public class TestCase2582292 extends AbstractMsalBrokerTest {
 
