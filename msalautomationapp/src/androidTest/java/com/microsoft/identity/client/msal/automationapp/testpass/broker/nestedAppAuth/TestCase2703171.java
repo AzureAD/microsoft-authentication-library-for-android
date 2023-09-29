1d87a5a6d6e8f1c48dc9ef0530c4ecfb1323814c
Sowmya Malayanur <69237821+somalaya@users.noreply.github.com>
2023-09-29 13:52:23 -0700

    Moving UI tests for NAA to msalautomationapp from brokerautomationapp (#1911)
    Moving UI tests for NAA to msalautomationapp from brokerautomationapp
    for below reasons
    
    - The tests added are for OneAuth and we do not have a OneAuth specific
    automation app. We usually have client specific tests in
    msalautomationapp. So I think these tests fit in msalautomationapp
    - We need @LocalBrokerHostDebugUiTest on all these tests as we need to
    specifically run these tests with brokerHost app (to be able to enable
    PRTV3 flight dynamically). Currently, in our pipeline, this annotation
    (LocalBrokerHostDebugUiTest) is only supported to run on
    msalautomationapp. So, the tests are failing. I have synced with Fadi
    and the pipeline can be modified in the future if we have a use case to
    run the tests annotated with LocalBrokerHostDebugUiTest in
    brokerautomationapp as well. But as of now, we did not take this
    approach, because the tests fit better with msalautomationapp.
    
    All the tests added here are from these PRs (I have simply moved the
    files)
    https://github.com/AzureAD/ad-accounts-for-android/pull/2523/files#diff-3082d89842e3922c2460fcd408fb44f2370f550bf1c433db305905f7e342c074
    and https://github.com/AzureAD/ad-accounts-for-android/pull/2555

---
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/nestedAppAuth/TestCase2703171.java
@@ -0,0 +1,96 @@
+//  Copyright (c) Microsoft Corporation.
+//  All rights reserved.
+//
+//  This code is licensed under the MIT License.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files(the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions :
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+package com.microsoft.identity.client.msal.automationapp.testpass.broker.nestedAppAuth;
+
+
+import com.microsoft.identity.client.msal.automationapp.R;
+import com.microsoft.identity.client.msal.automationapp.testpass.broker.AbstractMsalBrokerTest;
+import com.microsoft.identity.client.ui.automation.annotations.LocalBrokerHostDebugUiTest;
+import com.microsoft.identity.client.ui.automation.annotations.SupportedBrokers;
+import com.microsoft.identity.client.ui.automation.broker.BrokerHost;
+import com.microsoft.identity.common.java.dto.AccountRecord;
+import com.microsoft.identity.labapi.utilities.client.LabQuery;
+import com.microsoft.identity.labapi.utilities.constants.AzureEnvironment;
+import com.microsoft.identity.labapi.utilities.constants.TempUserType;
+import com.microsoft.identity.labapi.utilities.constants.UserType;
+
+import org.junit.Before;
+import org.junit.Test;
+
+// Nested app auth with US Gov accunt
+// https://identitydivision.visualstudio.com/Engineering/_workitems/edit/2702948
+@LocalBrokerHostDebugUiTest
+@SupportedBrokers(brokers = {BrokerHost.class})
+public class TestCase2703171 extends AbstractMsalBrokerTest {
+
+    @Before
+    public void before() {
+        ((BrokerHost) mBroker).enablePrtV3();
+    }
+    @Test
+    public void test_2703171() throws Throwable {
+        NestedAppHelper nestedAppHelper = new NestedAppHelper(mActivity, mLabAccount);
+        // perform AT interactive request for hub app
+        nestedAppHelper.performATForHubAppInUSGovCloud();
+
+        mBroker.forceStop();
+        mBroker.launch();
+
+        AccountRecord accountRecord = nestedAppHelper.getAccountRecordAfterHubAppAT();
+
+        try {
+            nestedAppHelper.performATSilentForNestedAppInUSGovCloud(accountRecord);
+
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    @Override
+    public LabQuery getLabQuery() {
+        return LabQuery.builder()
+                .userType(UserType.CLOUD)
+                .azureEnvironment(AzureEnvironment.AZURE_US_GOVERNMENT)
+                .build();
+    }
+
+    @Override
+    public TempUserType getTempUserType() {
+        return null;
+    }
+
+    @Override
+    public String[] getScopes() {
+        return new String[]{"User.read"};
+    }
+
+    @Override
+    public String getAuthority() {
+        return mApplication.getConfiguration().getDefaultAuthority().toString();
+    }
+
+    @Override
+    public int getConfigFileResourceId() {
+        return R.raw.msal_config_default;
+    }
+}
