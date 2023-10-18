0fd8a7820a2d9093d78c25c557862ab83a5c12ec
Dean Xu <cikelengfeng@gmail.com>
2023-10-18 15:31:57 +0800

    Add some new case for debug purpose

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2579095.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2579095.java
@@ -51,6 +51,8 @@ import com.microsoft.identity.client.ui.automation.utils.UiAutomatorUtils;
 import com.microsoft.identity.labapi.utilities.client.LabQuery;
 import com.microsoft.identity.labapi.utilities.constants.TempUserType;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -66,7 +68,10 @@ public class TestCase2579095 extends AbstractMsalBrokerTest {
             final String username = mLabAccount.getUsername();
             final String password = mLabAccount.getPassword();
 
+            // To make sure the device is in clean slate, uninstall mBroker here.
             mBroker.uninstall();
+            final MsalTestApp msalTestApp = new MsalTestApp();
+            msalTestApp.uninstall();
 
             // install legacy company portal
             final BrokerCompanyPortal brokerCompanyPortal = new BrokerCompanyPortal(BrokerCompanyPortal.OLD_COMPANY_PORTAL_APK,
@@ -102,8 +107,6 @@ public class TestCase2579095 extends AbstractMsalBrokerTest {
             brokerCompanyPortal.update();
 
             // install new MsalTestApp
-            final MsalTestApp msalTestApp = new MsalTestApp();
-            msalTestApp.uninstall();
             msalTestApp.install();
             msalTestApp.launch();
             msalTestApp.handleFirstRun();
@@ -129,7 +132,7 @@ public class TestCase2579095 extends AbstractMsalBrokerTest {
                     .build();
 
             msalTestApp.handleUserNameInput(username);
-            String tokenMsal = acquireTokenInMsalTestApp(msalTestApp, username, password, promptHandlerParametersMsal, null, false, false);
+            String tokenMsal = msalTestApp.acquireToken(username, password, promptHandlerParametersMsal, false);
             Assert.assertNotNull(tokenMsal);
 
             // getPackageName on MsalTestApp and should be Company Portal
@@ -137,49 +140,6 @@ public class TestCase2579095 extends AbstractMsalBrokerTest {
             final String activeBroker = msalTestApp.getActiveBrokerPackageName();
             Assert.assertEquals("Active broker pkg name : " + BrokerCompanyPortal.COMPANY_PORTAL_APP_PACKAGE_NAME, activeBroker);
         }
-
-    public String acquireTokenInMsalTestApp(@NonNull final MsalTestApp app,
-                                            @NonNull final String username,
-                               @NonNull final String password,
-                               @NonNull final PromptHandlerParameters promptHandlerParameters,
-                               @Nullable final IBrowser browser,
-                               final boolean shouldHandleBrowserFirstRun,
-                               @NonNull final boolean shouldHandlePrompt) throws UiObjectNotFoundException, InterruptedException {
-
-        final UiObject acquireTokenButton = UiAutomatorUtils.obtainUiObjectWithResourceId("com.msft.identity.client.sample.local:id/btn_acquiretoken", 15000);
-        scrollToElement(acquireTokenButton);
-        acquireTokenButton.click();
-
-        if (promptHandlerParameters.getBroker() == null && browser != null && shouldHandleBrowserFirstRun) {
-            // handle browser first run as applicable
-            browser.handleFirstRun();
-        }
-        // handle prompt if needed
-        if (shouldHandlePrompt) {
-            try {
-                final UiObject emailField = UiAutomatorUtils.obtainUiObjectWithTextAndClassType(
-                        "", EditText.class);
-                emailField.setText(username);
-                final UiObject nextBtn = UiAutomatorUtils.obtainUiObjectWithTextAndClassType(
-                        "Next", Button.class);
-                nextBtn.click();
-            } catch (final UiObjectNotFoundException e) {
-                throw new AssertionError("Could not click on object with txt Next");
-            }
-            final MicrosoftStsPromptHandler microsoftStsPromptHandler = new MicrosoftStsPromptHandler((MicrosoftStsPromptHandlerParameters) promptHandlerParameters);
-            microsoftStsPromptHandler.handlePrompt(username, password);
-        }
-
-        // get token and return
-        final UiObject result = UiAutomatorUtils.obtainUiObjectWithResourceId("com.msft.identity.client.sample.local:id/txt_result", 15000);
-        return result.getText();
-    }
-
-    private void scrollToElement(UiObject obj) throws UiObjectNotFoundException {
-        UiScrollable scrollable = new UiScrollable(new UiSelector().scrollable(true));
-        scrollable.scrollIntoView(obj);
-        obj.waitForExists(FIND_UI_ELEMENT_TIMEOUT);
-    }
 
     @Override
     public LabQuery getLabQuery() {
---
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2579095YixiBranch.java
@@ -0,0 +1,127 @@
+package com.microsoft.identity.client.msal.automationapp.testpass.broker.ltw;
+
+import com.microsoft.identity.client.msal.automationapp.R;
+import com.microsoft.identity.client.msal.automationapp.testpass.broker.AbstractMsalBrokerTest;
+import com.microsoft.identity.client.ui.automation.annotations.LTWTests;
+import com.microsoft.identity.client.ui.automation.annotations.RunOnAPI29Minus;
+import com.microsoft.identity.client.ui.automation.app.MsalTestApp;
+import com.microsoft.identity.client.ui.automation.app.OneAuthTestApp;
+import com.microsoft.identity.client.ui.automation.broker.BrokerCompanyPortal;
+import com.microsoft.identity.client.ui.automation.broker.BrokerMicrosoftAuthenticator;
+import com.microsoft.identity.client.ui.automation.interaction.FirstPartyAppPromptHandlerParameters;
+import com.microsoft.identity.client.ui.automation.interaction.PromptParameter;
+import com.microsoft.identity.client.ui.automation.interaction.microsoftsts.MicrosoftStsPromptHandlerParameters;
+import com.microsoft.identity.labapi.utilities.client.LabQuery;
+import com.microsoft.identity.labapi.utilities.constants.TempUserType;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+// Even Authenticator has the highest priority, if CP already has an artifact, CP will remain the broker.
+// https://identitydivision.visualstudio.com/Engineering/_workitems/edit/2579095
+@LTWTests
+@RunOnAPI29Minus
+public class TestCase2579095YixiBranch extends AbstractMsalBrokerTest {
+
+    @Test
+    public void test_2579095() throws Throwable {
+        final String username = mLabAccount.getUsername();
+        final String password = mLabAccount.getPassword();
+
+        // To make sure the device is in clean slate, uninstall mBroker here.
+        mBroker.uninstall();
+
+        // install legacy company portal
+        final BrokerCompanyPortal brokerCompanyPortal = new BrokerCompanyPortal(BrokerCompanyPortal.OLD_COMPANY_PORTAL_APK,
+                BrokerCompanyPortal.COMPANY_PORTAL_APK);
+        brokerCompanyPortal.install();
+
+        // install old OneAuthTestApp
+        final OneAuthTestApp oldOneAuthTestApp = new OneAuthTestApp();
+        oldOneAuthTestApp.installOldApk();
+        oldOneAuthTestApp.launch();
+        oldOneAuthTestApp.handleFirstRun();
+
+        // acquire token interactively on OneAuthTestApp
+        final FirstPartyAppPromptHandlerParameters promptHandlerParametersOneAuth = FirstPartyAppPromptHandlerParameters.builder()
+                .broker(mBroker)
+                .prompt(PromptParameter.LOGIN)
+                .loginHint(username)
+                .consentPageExpected(false)
+                .speedBumpExpected(false)
+                .sessionExpected(false)
+                .expectingBrokerAccountChooserActivity(false)
+                .expectingLoginPageAccountPicker(false)
+                .enrollPageExpected(false)
+                .build();
+        oldOneAuthTestApp.addFirstAccount(username, password, promptHandlerParametersOneAuth);
+        oldOneAuthTestApp.confirmAccount(username);
+
+        // install new Authenticator
+        final BrokerMicrosoftAuthenticator brokerMicrosoftAuthenticator = new BrokerMicrosoftAuthenticator();
+        brokerMicrosoftAuthenticator.install();
+
+        // update Company Portal
+        brokerCompanyPortal.update();
+
+        // install new MsalTestApp
+        final MsalTestApp msalTestApp = new MsalTestApp();
+        msalTestApp.install();
+        msalTestApp.launch();
+        msalTestApp.handleFirstRun();
+
+        // acquire token interactively on MsalTestApp and should not get prompt
+        final MicrosoftStsPromptHandlerParameters promptHandlerParametersMsal = MicrosoftStsPromptHandlerParameters.builder()
+                .prompt(PromptParameter.SELECT_ACCOUNT)
+                .loginHint(username)
+                .sessionExpected(false)
+                .broker(mBroker)
+                .expectingBrokerAccountChooserActivity(false)
+                .expectingProvidedAccountInBroker(false)
+                .expectingLoginPageAccountPicker(false)
+                .expectingProvidedAccountInCookie(false)
+                .consentPageExpected(false)
+                .passwordPageExpected(false)
+                .speedBumpExpected(false)
+                .registerPageExpected(false)
+                .enrollPageExpected(false)
+                .staySignedInPageExpected(false)
+                .verifyYourIdentityPageExpected(false)
+                .howWouldYouLikeToSignInExpected(false)
+                .build();
+
+        msalTestApp.handleUserNameInput(username);
+        String tokenMsal = msalTestApp.acquireToken(username, password, promptHandlerParametersMsal, false);
+        Assert.assertNotNull(tokenMsal);
+
+        // getPackageName on MsalTestApp and should be Company Portal
+        msalTestApp.handleBackButton();
+        final String activeBroker = msalTestApp.getActiveBrokerPackageName();
+        Assert.assertEquals("Active broker pkg name : " + BrokerCompanyPortal.COMPANY_PORTAL_APP_PACKAGE_NAME, activeBroker);
+    }
+
+    @Override
+    public LabQuery getLabQuery() {
+        return null;
+    }
+
+    @Override
+    public TempUserType getTempUserType() {
+        return TempUserType.BASIC;
+    }
+
+    @Override
+    public String[] getScopes() {
+        return new String[]{"User.read"};
+    }
+
+    @Override
+    public String getAuthority() {
+        return mApplication.getConfiguration().getDefaultAuthority().getAuthorityURL().toString();
+    }
+
+    @Override
+    public int getConfigFileResourceId() {
+        return R.raw.msal_config_default;
+    }
+}
