c5c1f3db14ab8c0af1723665284a88bdd7e579c1
Yixi Lin <yixilin@microsoft.com>
2023-10-26 13:52:21 +0800

    add retry to prevent network issue

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2584410.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2584410.java
@@ -25,6 +25,7 @@ package com.microsoft.identity.client.msal.automationapp.testpass.broker.ltw;
 import com.microsoft.identity.client.msal.automationapp.R;
 import com.microsoft.identity.client.msal.automationapp.testpass.broker.AbstractMsalBrokerTest;
 import com.microsoft.identity.client.ui.automation.annotations.LTWTests;
+import com.microsoft.identity.client.ui.automation.annotations.RetryOnFailure;
 import com.microsoft.identity.client.ui.automation.annotations.SupportedBrokers;
 import com.microsoft.identity.client.ui.automation.app.MsalTestApp;
 import com.microsoft.identity.client.ui.automation.broker.BrokerCompanyPortal;
@@ -41,6 +42,7 @@ import org.junit.Test;
 // Authenticator has highest priority  - Case3 (Auth, LTW, CP)
 // https://identitydivision.visualstudio.com/Engineering/_workitems/edit/2584410
 @LTWTests
+@RetryOnFailure
 @SupportedBrokers(brokers = {BrokerMicrosoftAuthenticator.class})
 public class TestCase2584410 extends AbstractMsalBrokerTest {
     @Test
