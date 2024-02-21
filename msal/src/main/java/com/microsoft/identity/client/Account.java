08844c960478fe5345f46df055351c4416cdfc89
SammyO <1315628+SammyO@users.noreply.github.com>
2024-02-21 16:57:42 +0000

    Add ADO ticket in comments

--- msal/src/main/java/com/microsoft/identity/client/Account.java
+++ msal/src/main/java/com/microsoft/identity/client/Account.java
@@ -180,7 +180,8 @@ public class Account implements IAccount {
             if (!StringUtil.isEmpty(iss)) {
                 // We're using the issuer to determine whether this is a CIAM authority, as the environment
                 // be a custom-URL-domain variant, which won't contain ciamlogin.com.
-                // Note: this will work as long as the issuer for CUD doesn't change.
+                // Note: this will work as long as the issuer for CUD doesn't change, which it will
+                // in the future. See https://identitydivision.visualstudio.com/Engineering/_workitems/edit/2832277
                 if (iss.contains(CIAMAuthority.CIAM_LOGIN_URL_SEGMENT) && !StringUtil.isEmpty(getEnvironment())
                         && !StringUtil.isEmpty(getTenantId())) {
                     // Call static method in CIAMAuthority to create the full authority uri
