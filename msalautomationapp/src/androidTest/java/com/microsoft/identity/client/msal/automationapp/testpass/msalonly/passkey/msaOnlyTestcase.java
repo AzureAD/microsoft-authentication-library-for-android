dffac81121928b609d7655a1b5451898766bf85b
Melissa Ahn <melissaahn@microsoft.com>
2024-05-23 10:16:05 -0700

    saving progress

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/msalonly/passkey/msaOnlyTestcase.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/msalonly/passkey/msaOnlyTestcase.java
@@ -24,16 +24,28 @@ package com.microsoft.identity.client.msal.automationapp.testpass.msalonly.passk
 
 import android.widget.EditText;
 
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.uiautomator.UiDevice;
+
 import com.microsoft.identity.client.msal.automationapp.AbstractMsalUiTest;
 import com.microsoft.identity.client.msal.automationapp.R;
 import com.microsoft.identity.client.ui.automation.browser.BrowserChrome;
+import com.microsoft.identity.client.ui.automation.constants.GlobalConstants;
 import com.microsoft.identity.client.ui.automation.utils.UiAutomatorUtils;
+import com.microsoft.identity.common.java.util.ThreadUtils;
 import com.microsoft.identity.labapi.utilities.client.LabQuery;
 import com.microsoft.identity.labapi.utilities.constants.TempUserType;
+import com.microsoft.identity.labapi.utilities.constants.UserType;
 
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class msaOnlyTestcase extends AbstractMsalUiTest {
+    String TAG = this.getClass().getSimpleName();
+    // If running locally, the systemPin variable should be updated to your device's PIN.
+    // Otherwise, the DevicePinSetupRule sets the PIN to a GlobalConstant.
+    String systemPin = "2468";
 
     @Test
     public void msa_testcase() throws Throwable {
@@ -45,17 +57,34 @@ public class msaOnlyTestcase extends AbstractMsalUiTest {
         chrome.launch();
         UiAutomatorUtils.handleButtonClickForObjectWithText("Use without an account");
         UiAutomatorUtils.handleButtonClickForObjectWithText("Got it");
-        chrome.navigateTo("account.live.com");
-        UiAutomatorUtils.handleButtonClickForObjectWithText("Sign in to your Microsoft account");
+        chrome.navigateTo("https://go.microsoft.com/fwlink/?linkid=2250685");
+        //UiAutomatorUtils.handleButtonClickForObjectWithTextSafely("Sign in to your Microsoft account");
         UiAutomatorUtils.obtainUiObjectWithClassAndIndex(EditText.class, 0).setText(username);
-        UiAutomatorUtils.handleButtonClickForObjectWithText("Next");
+        UiAutomatorUtils.handleButtonClickForObjectWithTextSafely("Next");
+        UiAutomatorUtils.pressBack();
         UiAutomatorUtils.obtainUiObjectWithClassAndIndex(EditText.class, 0).setText(password);
+        final UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        ThreadUtils.sleepSafely((int) TimeUnit.SECONDS.toMillis(2), TAG, "Wait some seconds for next screen.");
+        device.click(880, 1044);
+        UiAutomatorUtils.handleButtonClickForObjectWithTextSafely("Email");
+        ThreadUtils.sleepSafely((int) TimeUnit.SECONDS.toMillis(40), TAG, "Wait 30 seconds to finish email MFA.");
+        //UiAutomatorUtils.handleButtonClickForObjectWithTextSafely("Sign in");
+        //UiAutomatorUtils.pressEnter();
+        //UiAutomatorUtils.handleButtonClickForObjectWithText("No");
+        //ThreadUtils.sleepSafely((int) TimeUnit.SECONDS.toMillis(10), TAG, "Wait some seconds for next screen.");
+        device.click(480, 577);
+        UiAutomatorUtils.handleButtonClickSafely("com.google.android.gms:id/continue_button");
+        UiAutomatorUtils.handleInput("com.android.systemui:id/lockPassword", systemPin);
         UiAutomatorUtils.pressEnter();
+        ThreadUtils.sleepSafely((int)TimeUnit.SECONDS.toMillis(15), TAG, "Sleeping while adding a passkey");
+
     }
 
     @Override
     public LabQuery getLabQuery() {
-        return null;
+        return LabQuery.builder()
+                .userType(UserType.MSA)
+                .build();
     }
 
     @Override
