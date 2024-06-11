57ab98adb0d184f95920c9f48c5816ea31f260cd
Melissa Ahn <melissaahn@microsoft.com>
2024-06-11 16:34:56 -0700

    cleaned up

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/passkey/TestCase2801802.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/passkey/TestCase2801802.java
@@ -25,9 +25,6 @@ package com.microsoft.identity.client.msal.automationapp.testpass.broker.passkey
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.uiautomator.UiDevice;
-
 import com.microsoft.identity.client.Prompt;
 import com.microsoft.identity.client.msal.automationapp.R;
 import com.microsoft.identity.client.msal.automationapp.sdk.MsalAuthResult;
@@ -35,7 +32,6 @@ import com.microsoft.identity.client.msal.automationapp.sdk.MsalAuthTestParams;
 import com.microsoft.identity.client.msal.automationapp.sdk.MsalSdk;
 import com.microsoft.identity.client.msal.automationapp.testpass.broker.AbstractMsalBrokerTest;
 import com.microsoft.identity.client.ui.automation.TokenRequestTimeout;
-import com.microsoft.identity.client.ui.automation.constants.GlobalConstants;
 import com.microsoft.identity.client.ui.automation.device.settings.GoogleSettings;
 import com.microsoft.identity.client.ui.automation.interaction.OnInteractionRequired;
 import com.microsoft.identity.client.ui.automation.interaction.PromptHandlerParameters;
@@ -47,105 +43,31 @@ import com.microsoft.identity.labapi.utilities.client.LabQuery;
 import com.microsoft.identity.labapi.utilities.constants.AzureEnvironment;
 import com.microsoft.identity.labapi.utilities.constants.TempUserType;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
+// https://identitydivision.visualstudio.com/Engineering/_workitems/edit/2801802
+// Please note that this test is meant to be run locally.
+// You must update the username, password, and systemPin variables to suit your own test account and device.
+// Comment out @Ignore before running.
+// Remember to delete your passkey in Authenticator and from your account at the conclusion of this test.
+@Ignore("Ignoring as this test is not able to be run on the pipeline yet. This line can be commented out when running locally.")
 public class TestCase2801802 extends AbstractMsalBrokerTest {
     String TAG = this.getClass().getSimpleName();
-    // If running locally, the systemPin variable should be updated to your device's PIN.
-    // Otherwise, the DevicePinSetupRule sets the PIN to a GlobalConstant.
-    String systemPin = GlobalConstants.PIN;
+
+    // This test is currently running with a username and tap because the test accounts use TAP to register a passkey.
+    // Once we can get Lab accounts to have passkeys enabled, we can switch over to using those accounts and running this test on the pipeline.
     @Test
     public void test_2801802() throws Throwable {
-        final String username = mLabAccount.getUsername();
-        final String password = mLabAccount.getPassword();
-
-        // Enable Authenticator as a passkey provider.
-        final GoogleSettings settings = new GoogleSettings();
-        settings.launchAccountListPage();
-        UiAutomatorUtils.handleButtonClick("com.android.settings:id/switchWidget");
-
-        // Add an account to AuthApp.
-        mBroker.launch();
-        UiAutomatorUtils.handleButtonClickSafely("com.android.permissioncontroller:id/permission_allow_button");
-        mBroker.handleFirstRun();
-        UiAutomatorUtils.handleButtonClickSafely("com.azure.authenticator:id/zero_accounts_add_account_button");
-        UiAutomatorUtils.handleButtonClickSafely("com.azure.authenticator:id/add_account_work_btn");
-        UiAutomatorUtils.obtainUiObjectWithClassAndDescription(LinearLayout.class, "Sign in 2 of 2").click();
-        UiAutomatorUtils.obtainUiObjectWithClassAndIndex(EditText.class, 0).setText(username);
-        UiAutomatorUtils.handleButtonClickForObjectWithTextSafely("Next");
-        UiAutomatorUtils.obtainUiObjectWithClassAndIndex(EditText.class, 0).setText(password);
-        //UiAutomatorUtils.handleInput("accesspass", password);
-        UiAutomatorUtils.pressEnter();
-        // Adding account requires some extra linking steps.
-        // Can't find the next few objects to click using conventional methods, so need to use coordinates.
-        ThreadUtils.sleepSafely((int)TimeUnit.SECONDS.toMillis(5), TAG, "Wait a few seconds for next screen.");
-        final UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-        device.click(800, 850);
-        ThreadUtils.sleepSafely((int)TimeUnit.SECONDS.toMillis(10), TAG, "Wait a few seconds for next screen.");
-        device.click(820, 1275);
-        ThreadUtils.sleepSafely((int)TimeUnit.SECONDS.toMillis(5), TAG, "Wait a few seconds for next screen.");
-        UiAutomatorUtils.handleButtonClickForObjectWithTextSafely("Pair your account to the app by clicking this link.");
-        UiAutomatorUtils.handleButtonClickSafely("android:id/button1");
-
-        // TODO: below should be steps to register a passkey.
-        // Currently these lines are based on adding an AuthApp account, but they will need to be altered when lab accounts have passkeys enabled.
-        UiAutomatorUtils.handleButtonClickSafely("com.azure.authenticator:id/account_list_row_account_name");
-        //UiAutomatorUtils.obtainUiObjectWithTextAndClassType("Continue", TextView.class).click();
-        //UiAutomatorUtils.handleButtonClickSafely("idSIButton9");
-        UiAutomatorUtils.handleInput("com.android.systemui:id/lockPassword", systemPin);
-        UiAutomatorUtils.pressEnter();
-        ThreadUtils.sleepSafely((int)TimeUnit.SECONDS.toMillis(15), TAG, "Sleeping while registering");
-        UiAutomatorUtils.handleButtonClickForObjectWithText("Done");
-        UiAutomatorUtils.handleInput("com.android.systemui:id/lockPassword", systemPin);
-        UiAutomatorUtils.pressEnter();
-
-        ThreadUtils.sleepSafely((int)TimeUnit.SECONDS.toMillis(15), TAG, "Sleeping while adding a passkey");
-
-        // Now try to use the passkey to login.
-        // TODO: sign in with UPN and without UPN.
-        final MsalSdk msalSdk = new MsalSdk();
-        final MsalAuthTestParams authTestParams = MsalAuthTestParams.builder()
-                .activity(mActivity)
-                .loginHint(username)
-                .scopes(Arrays.asList(mScopes))
-                .promptParameter(Prompt.SELECT_ACCOUNT)
-                .msalConfigResourceId(getConfigFileResourceId())
-                .build();
-
-        final MsalAuthResult authResult;
-        try {
-            authResult = msalSdk.acquireTokenInteractive(authTestParams, new OnInteractionRequired() {
-                @Override
-                public void handleUserInteraction() {
-                    final PromptHandlerParameters promptHandlerParameters = PromptHandlerParameters.builder()
-                            .prompt(PromptParameter.SELECT_ACCOUNT)
-                            .loginHint(username)
-                            .sessionExpected(false)
-                            .consentPageExpected(false)
-                            .speedBumpExpected(false)
-                            .broker(mBroker)
-                            .passwordPageExpected(false)
-                            .expectingBrokerAccountChooserActivity(false)
-                            .choosePasskeyExpected(true)
-                            .build();
-
-                    new AadPromptHandler(promptHandlerParameters)
-                            .handlePrompt(username, "no password");
-                }
-            }, TokenRequestTimeout.MEDIUM);
-            authResult.assertSuccess();
-        } catch (Throwable e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    public void test_2801802_tap() throws Throwable {
-        final String username = "username";
-        final String tap = "tap";
+        // Update the username and tap values.
+        final String username = "PUT USERNAME HERE";
+        final String tap = "PUT TAP HERE";
+        // The systemPin variable should be updated to your device's PIN.
+        // In the future, when this test gets added to the pipeline, the DevicePinSetupRule sets the PIN to a GlobalConstant.
+        String systemPin = "1234";
 
         // Enable Authenticator as a passkey provider.
         final GoogleSettings settings = new GoogleSettings();
