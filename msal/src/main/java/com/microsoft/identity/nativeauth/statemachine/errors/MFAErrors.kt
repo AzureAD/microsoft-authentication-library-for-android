b8b1535c5290eccad8b67a61838b5f3c37448ad1
SammyO <1315628+SammyO@users.noreply.github.com>
2024-09-18 14:27:54 +0100

    Create dedicated error classes for MFA

--- msal/src/main/java/com/microsoft/identity/nativeauth/statemachine/errors/MFAErrors.kt
+++ msal/src/main/java/com/microsoft/identity/nativeauth/statemachine/errors/MFAErrors.kt
@@ -5,9 +5,10 @@ import com.microsoft.identity.nativeauth.statemachine.results.MFARequiredResult
 import com.microsoft.identity.nativeauth.statemachine.results.MFASubmitChallengeResult
 
 /**
- * MFA error. The user should use the utility methods of this class
- * to identify and handle the error. This error is produced by the various
- * MFA operations.
+ * MFA error. Use the utility methods of this class
+ * to identify and handle the error. This error is produced by
+ * [com.microsoft.identity.nativeauth.statemachine.states.MFARequiredState.requestChallenge] and
+ * [com.microsoft.identity.nativeauth.statemachine.states.AwaitingMFAState.requestChallenge].
  * @param errorType the error type value of the error that occurred.
  * @param error the error returned by the authentication server.
  * @param errorMessage the error message returned by the authentication server.
@@ -15,14 +16,36 @@ import com.microsoft.identity.nativeauth.statemachine.results.MFASubmitChallenge
  * @param errorCodes a list of specific error codes returned by the authentication server.
  * @param exception an internal unexpected exception that happened.
  */
-class MFAError(
+class MFARequestChallengeError(
     override val errorType: String? = null,
     override val error: String? = null,
     override val errorMessage: String?,
     override val correlationId: String,
     override val errorCodes: List<Int>? = null,
+    val subError: String? = null,
     override var exception: Exception? = null
-): MFARequiredResult, BrowserRequiredError, MFAGetAuthMethodsResult, Error(errorType = errorType, error = error, errorMessage= errorMessage, correlationId = correlationId, errorCodes = errorCodes, exception = exception)
+): MFARequiredResult, BrowserRequiredError, Error(errorType = errorType, error = error, errorMessage= errorMessage, correlationId = correlationId, errorCodes = errorCodes, exception = exception)
+
+/**
+ * MFA error. Use the utility methods of this class
+ * to identify and handle the error. This error is produced by
+ * [com.microsoft.identity.nativeauth.statemachine.states.MFARequiredState.getAuthMethods]
+ * @param errorType the error type value of the error that occurred.
+ * @param error the error returned by the authentication server.
+ * @param errorMessage the error message returned by the authentication server.
+ * @param correlationId a unique identifier for the request that can help in diagnostics.
+ * @param errorCodes a list of specific error codes returned by the authentication server.
+ * @param exception an internal unexpected exception that happened.
+ */
+class GetAuthMethodsError(
+    override val errorType: String? = null,
+    override val error: String? = null,
+    override val errorMessage: String?,
+    override val correlationId: String,
+    override val errorCodes: List<Int>? = null,
+    val subError: String? = null,
+    override var exception: Exception? = null
+): MFAGetAuthMethodsResult, BrowserRequiredError, Error(errorType = errorType, error = error, errorMessage= errorMessage, correlationId = correlationId, errorCodes = errorCodes, exception = exception)
 
 /**
  * MFA error. The user should use the utility methods of this class
@@ -35,7 +58,7 @@ class MFAError(
  * @param errorCodes a list of specific error codes returned by the authentication server.
  * @param exception an internal unexpected exception that happened.
  */
-class SubmitChallengeError(
+class MFASubmitChallengeError(
     override val errorType: String? = null,
     override val error: String? = null,
     override val errorMessage: String?,
