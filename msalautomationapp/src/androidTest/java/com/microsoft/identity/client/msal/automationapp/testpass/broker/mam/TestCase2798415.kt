b84ebabdb8251d4a0632fc86df9670e7885f35c2
fadidurah <fadidurah@microsoft.com>
2024-10-21 02:04:33 -0400

    remove press back

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/mam/TestCase2798415.kt
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/mam/TestCase2798415.kt
@@ -35,7 +35,6 @@ import com.microsoft.identity.client.ui.automation.installer.LocalApkInstaller
 import com.microsoft.identity.client.ui.automation.interaction.FirstPartyAppPromptHandlerParameters
 import com.microsoft.identity.client.ui.automation.interaction.PromptParameter
 import com.microsoft.identity.client.ui.automation.logging.Logger
-import com.microsoft.identity.client.ui.automation.utils.UiAutomatorUtils
 import com.microsoft.identity.labapi.utilities.client.ILabAccount
 import com.microsoft.identity.labapi.utilities.client.LabQuery
 import com.microsoft.identity.labapi.utilities.constants.ProtectionPolicy
@@ -69,7 +68,6 @@ class TestCase2798415 : AbstractMsalBrokerTest() {
         teams.install()
         teams.launch()
         teams.handleFirstRun()
-        UiAutomatorUtils.pressBack()
 
         val username = mLabAccount.username
         val password = mLabAccount.password
