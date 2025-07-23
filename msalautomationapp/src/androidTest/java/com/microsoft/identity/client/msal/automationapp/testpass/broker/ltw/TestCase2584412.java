47938f033161ff26b2310f8804c10afd4397c17a
Dome Pongmongkol <rapong@microsoft.com>
2025-07-23 10:51:59 -0700

    ltwWithGradleUpgrade changes

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2584412.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2584412.java
@@ -52,8 +52,8 @@ import java.util.List;
 // https://identitydivision.visualstudio.com/Engineering/_workitems/edit/2584412
 @LTWTests
 @SupportedBrokers(brokers = {BrokerCompanyPortal.class})
-@RetryOnFailure
 @RunWith(Parameterized.class)
+@RetryOnFailure
 public class TestCase2584412 extends AbstractMsalBrokerTest {
     private final UserType mUserType;
 
