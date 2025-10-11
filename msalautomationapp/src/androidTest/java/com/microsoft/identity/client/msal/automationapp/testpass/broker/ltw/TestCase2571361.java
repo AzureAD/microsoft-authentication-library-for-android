33e2d7bd0e2e9dc8cec2c6bb22276474c5d8f0ea
fadidurah <fadidurah@microsoft.com>
2025-10-11 15:45:20 -0400

    add ltw changes, no api 29

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2571361.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2571361.java
@@ -28,7 +28,6 @@ import com.microsoft.identity.client.msal.automationapp.R;
 import com.microsoft.identity.client.msal.automationapp.testpass.broker.AbstractMsalBrokerTest;
 import com.microsoft.identity.client.ui.automation.annotations.LTWTests;
 import com.microsoft.identity.client.ui.automation.annotations.RetryOnFailure;
-import com.microsoft.identity.client.ui.automation.annotations.RunOnAPI29Minus;
 import com.microsoft.identity.client.ui.automation.annotations.SupportedBrokers;
 import com.microsoft.identity.client.ui.automation.app.MsalTestApp;
 import com.microsoft.identity.client.ui.automation.app.OneAuthTestApp;
@@ -52,7 +51,6 @@ import java.util.List;
 // If Company Portal is installed after LTW, user should still get SSO
 // https://identitydivision.visualstudio.com/Engineering/_workitems/edit/2571361
 @LTWTests
-@RunOnAPI29Minus
 @RetryOnFailure
 @SupportedBrokers(brokers = {BrokerLTW.class})
 @RunWith(Parameterized.class)
