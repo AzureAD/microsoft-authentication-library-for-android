1c971701bf92931da84cb54c128e453048373d9b
p3dr0rv <pedroro@microsoft.com>
2026-03-06 22:16:26 -0800

    Refactor shared device registration method in TestCase2582292

--- msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2582292.java
+++ msalautomationapp/src/androidTest/java/com/microsoft/identity/client/msal/automationapp/testpass/broker/ltw/TestCase2582292.java
@@ -77,7 +77,7 @@ public class TestCase2582292 extends AbstractMsalBrokerTest {
         brokerHost.launch();
 
         // In brokerHost Multiple WPJ mode: perform a shared device registration with a cloud device admin account from the LAB API
-        brokerHost.multipleWpjApiFragment.performSharedDeviceRegistration(username1, password1);
+        brokerHost.performSharedDeviceRegistration(username1, password1);
 
         // Uninstall BrokerHost App
         brokerHost.uninstall();
